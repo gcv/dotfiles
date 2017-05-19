060fa4d40bc77b113eba4d564ca0ab19d33bda57
Constantine Vetoshev <vetoshev@gmail.com>
2017-05-18 18:56:28 -0700

    Add a script to automatically install Homebrew packages.

---
+++ brew.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+
+if [[ ${UNAME} != "Darwin" ]]; then
+    echo "do not run this on anything other than macOS!"
+    exit 1
+fi
+
+
+BREW_HOME=/opt/brew
+
+if [[ ! -d "$BREW_HOME" ]]; then
+
+    GITHUB_APP_GIT=/Applications/GitHub.app/Contents/Resources/git/bin/git
+    if [[ ! -f $GITHUB_APP_GIT ]]; then
+        echo "download the GitHub app and try again"
+        exit 1
+    fi
+
+    sudo mkdir -p "${BREW_HOME}"
+    sudo chown -R ${USER}:wheel "${BREW_HOME}"
+
+    ${GITHUB_APP_GIT} clone git://github.com/Homebrew/brew.git "${BREW_HOME}"
+
+fi
+
+
+${BREW_HOME}/bin/brew install \
+     aspell \
+     autoconf \
+     autojump \
+     automake \
+     awscli \
+     bash \
+     boost \
+     cmake \
+     coreutils \
+     ctags \
+     docker \
+     docker-machine \
+     findutils \
+     git \
+     global \
+     gnupg@1.4 \
+     gsl \
+     heroku \
+     icu4c \
+     ledger \
+     libtool \
+     makedepend \
+     maven \
+     nmap \
+     offlineimap \
+     pkg-config \
+     pv \
+     python3 \
+     rlwrap \
+     roswell \
+     sqlite \
+     swig \
+     taglib \
+     the_silver_searcher \
+     tmux \
+     tree \
+     watch \
+     wget \
+     xz \
+     youtube-dl \
+     zsh
