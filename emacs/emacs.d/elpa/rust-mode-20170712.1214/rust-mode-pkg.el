98e4eafaaddf2cbad51255c03f2bd3642dfb2a96
Constantine Vetoshev <vetoshev@gmail.com>
2017-07-16 13:33:12 -0700

    MELPA updates.

---
+++ emacs/emacs.d/elpa/rust-mode-20170712.1214/rust-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "rust-mode" "20170712.1214" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :commit "60a1f36f4111e825d20d9c3aed561981c470806a" :url "https://github.com/rust-lang/rust-mode" :keywords '("languages"))
