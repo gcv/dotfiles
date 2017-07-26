47f734e76404c8b20132443c0ca652b246b585f6
Constantine Vetoshev <vetoshev@gmail.com>
2017-07-26 19:23:18 -0400

    MELPA upgrades.

---
+++ emacs/emacs.d/elpa/counsel-20170726.1022/counsel-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel" "20170726.1022" "Various completion functions using Ivy" '((emacs "24.3") (swiper "0.9.0")) :commit "714f9f83803a7a1429e3a2e38dab3510cafe9cfb" :url "https://github.com/abo-abo/swiper" :keywords '("completion" "matching"))
