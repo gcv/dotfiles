ee08e0292602ab8eba1dd03b808d3c85e7685ca7
Constantine Vetoshev <vetoshev@gmail.com>
2017-09-05 12:47:05 -0700

    MELPA updates.

---
+++ emacs/emacs.d/elpa/queue-0.2/queue-autoloads.el
@@ -0,0 +1,23 @@
+;;; queue-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "queue" "queue.el" (22958 65275 0 0))
+;;; Generated autoloads from queue.el
+
+(defalias 'make-queue 'queue-create "\
+Create an empty queue data structure.")
+
+;;;***
+
+;;;### (autoloads nil nil ("queue-pkg.el") (22958 65275 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; queue-autoloads.el ends here
--- emacs/emacs.d/elpa/which-key-3.0.1/which-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "which-key" "3.0.1" "Display available keybindings in popup" '((emacs "24.4")) :commit "3ff303b50495d492cfac70cc9f7321971928bdb1" :url "https://github.com/justbur/emacs-which-key")
