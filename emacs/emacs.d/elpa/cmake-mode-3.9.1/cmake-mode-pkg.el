cddb3c20cc9784918428c90337971152cdfef830
Constantine Vetoshev <vetoshev@gmail.com>
2017-09-23 08:10:15 -0700

    MELPA upgrades.

--- emacs/emacs.d/elpa/cmake-mode-3.9.1/cmake-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "cmake-mode" "3.9.1" "major-mode for editing CMake sources" 'nil :commit "fca4423786ba2c4a5ab0ec6c1a1cbac8cd8600b4")
---
+++ emacs/emacs.d/elpa/cmake-mode-3.9.3/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "3.9.3" "major-mode for editing CMake sources" 'nil :commit "7486607c874de3e44d33f152775fd073de2ba2c0")
