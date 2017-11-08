ab61055456b9c5fbcf7c282c119597764dc2ab48
Constantine Vetoshev <vetoshev@gmail.com>
2017-11-08 10:23:47 -0800

    Stop restoring windows after a hotkey-driven resize.

--- public/hammerspoon/init.lua
+++ public/hammerspoon/init.lua
@@ -106,70 +106,54 @@ end
 hs.hotkey.bind(
    {"ctrl", "alt", "cmd"}, "left",
    function()
-      alterOrRestoreFrame(
-         hs.window.focusedWindow(),
-         function(win)
-            local f = win:frame()
-            local screen = win:screen()
-            local max = screen:frame()
-            f.x = max.x
-            f.y = max.y
-            f.w = max.w / 2
-            f.h = max.h
-            win:setFrame(f)
-         end
-      )
+      local win = hs.window.focusedWindow()
+      local f = win:frame()
+      local screen = win:screen()
+      local max = screen:frame()
+      f.x = max.x
+      f.y = max.y
+      f.w = max.w / 2
+      f.h = max.h
+      win:setFrame(f)
    end
 )
 
 hs.hotkey.bind(
    {"ctrl", "alt", "cmd"}, "right",
    function()
-      alterOrRestoreFrame(
-         hs.window.focusedWindow(),
-         function(win)
-            local f = win:frame()
-            local screen = win:screen()
-            local max = screen:frame()
-            f.x = max.x + (max.w / 2)
-            f.y = max.y
-            f.w = max.w / 2
-            f.h = max.h
-            win:setFrame(f)
-         end
-      )
+      local win = hs.window.focusedWindow()
+      local f = win:frame()
+      local screen = win:screen()
+      local max = screen:frame()
+      f.x = max.x + (max.w / 2)
+      f.y = max.y
+      f.w = max.w / 2
+      f.h = max.h
+      win:setFrame(f)
    end
 )
 
 hs.hotkey.bind(
    {"ctrl", "alt", "cmd"}, "up",
    function()
-      alterOrRestoreFrame(
-         hs.window.focusedWindow(),
-         function(win)
-            local f = win:frame()
-            local screen = win:screen()
-            local max = screen:frame()
-            frameFillMostOfScreenUp(f, max)
-            win:setFrame(f)
-         end
-      )
+      local win = hs.window.focusedWindow()
+      local f = win:frame()
+      local screen = win:screen()
+      local max = screen:frame()
+      frameFillMostOfScreenUp(f, max)
+      win:setFrame(f)
    end
 )
 
 hs.hotkey.bind(
    {"ctrl", "alt", "cmd"}, "down",
    function()
-      alterOrRestoreFrame(
-         hs.window.focusedWindow(),
-         function(win)
-            local f = win:frame()
-            local screen = win:screen()
-            local max = screen:frame()
-            frameFillMostOfScreenDown(f, max)
-            win:setFrame(f)
-         end
-      )
+      local win = hs.window.focusedWindow()
+      local f = win:frame()
+      local screen = win:screen()
+      local max = screen:frame()
+      frameFillMostOfScreenDown(f, max)
+      win:setFrame(f)
    end
 )
 
@@ -177,49 +161,37 @@ function makeScreenFillHotkeysForScreen(keyString, screenId)
    hs.hotkey.bind(
       {"alt", "cmd"}, keyString,
       function()
-         alterOrRestoreFrame(
-            hs.window.focusedWindow(),
-            function(win)
-               local f = win:frame()
-               local screen = hs.screen.allScreens()[screenId]
-               local max = screen:frame()
-               frameFillMostOfScreenUp(f, max)
-               win:setFrame(f)
-            end
-         )
+         local win = hs.window.focusedWindow()
+         local f = win:frame()
+         local screen = hs.screen.allScreens()[screenId]
+         local max = screen:frame()
+         frameFillMostOfScreenUp(f, max)
+         win:setFrame(f)
       end
    )
    hs.hotkey.bind(
       {"ctrl", "alt", "cmd"}, keyString,
       function()
-         alterOrRestoreFrame(
-            hs.window.focusedWindow(),
-            function(win)
-               local f = win:frame()
-               local screen = hs.screen.allScreens()[screenId]
-               local max = screen:frame()
-               frameFillMostOfScreenDown(f, max)
-               win:setFrame(f)
-            end
-         )
+         local win = hs.window.focusedWindow()
+         local f = win:frame()
+         local screen = hs.screen.allScreens()[screenId]
+         local max = screen:frame()
+         frameFillMostOfScreenDown(f, max)
+         win:setFrame(f)
       end
    )
    hs.hotkey.bind(
       {"shift", "alt", "cmd"}, keyString,
       function()
-         alterOrRestoreFrame(
-            hs.window.focusedWindow(),
-            function(win)
-               local f = win:frame()
-               local screen = hs.screen.allScreens()[screenId]
-               local max = screen:frame()
-               f.x = max.x
-               f.y = max.y
-               f.w = (max.w / 8) * 7
-               f.h = max.h
-               win:setFrame(f)
-            end
-         )
+         local win = hs.window.focusedWindow()
+         local f = win:frame()
+         local screen = hs.screen.allScreens()[screenId]
+         local max = screen:frame()
+         f.x = max.x
+         f.y = max.y
+         f.w = (max.w / 8) * 7
+         f.h = max.h
+         win:setFrame(f)
       end
    )
 end
