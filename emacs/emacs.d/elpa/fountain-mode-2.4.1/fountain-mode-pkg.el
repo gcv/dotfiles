49945594ddfb3519e87f40d82c3cf1b9239eef0a
Constantine Vetoshev <vetoshev@gmail.com>
2018-02-22 20:20:52 -0800

    MELPA updates.

--- emacs/emacs.d/elpa/fountain-mode-2.4.1/fountain-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "fountain-mode" "2.4.1" "Major mode for screenwriting in Fountain markup" '((emacs "24.5")) :commit "f1dc9dff6779c0ce6ab0a1c0ae349df1194a314f" :url "https://github.com/rnkn/fountain-mode" :keywords '("wp"))
---
+++ emacs/emacs.d/elpa/fountain-mode-2.4.2/fountain-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "fountain-mode" "2.4.2" "Major mode for screenwriting in Fountain markup" '((emacs "24.5")) :commit "e2878da13e7b87a824ebd6c842e9f552369b220c" :url "https://github.com/rnkn/fountain-mode" :keywords '("wp"))
