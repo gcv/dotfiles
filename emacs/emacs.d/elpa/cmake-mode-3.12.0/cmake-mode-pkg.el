6bbda8a4f76b4746a6a04f31b906e396de32c4d3
Constantine Vetoshev <vetoshev@gmail.com>
2018-07-17 09:11:48 -0700

    MELPA updates.

---
+++ emacs/emacs.d/elpa/cmake-mode-3.12.0/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "3.12.0" "major-mode for editing CMake sources" 'nil)
---
+++ emacs/emacs.d/elpa/perspective-2.1/perspective-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "perspective" "2.1" "switch between named \"perspectives\" of the editor" '((cl-lib "0.5")))
