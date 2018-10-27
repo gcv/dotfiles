8d0ea14e5b1c4d007ed2ee1d3cdb3b4567b53bde
Constantine Vetoshev <vetoshev@gmail.com>
2018-10-27 14:33:15 -0700

    Missed a hook refactor.

--- emacs/elisp.el
+++ emacs/elisp.el
@@ -1,28 +1,35 @@
 (put 'add-hook 'lisp-indent-function 1)
 (put 'setq-default 'lisp-indent-function 1)
 
+
 (diminish 'eldoc-mode)
 
-(add-hook 'emacs-lisp-mode-hook
-  (lambda ()
-    (setq show-trailing-whitespace t)
-    (paredit-mode 1)
-    (eldoc-mode)
-    (elisp-slime-nav-mode t)
-    (define-key emacs-lisp-mode-map (kbd "C-m") 'newline-and-indent)))
 
-(add-hook 'lisp-interaction-mode-hook
-  (lambda ()
-    (paredit-mode 1)
-    (define-key lisp-interaction-mode-map (kbd "C-x C-j") 'eval-print-last-sexp)
-    (eldoc-mode)))
+(defun /emacs-lisp-mode-hook ()
+  (setq show-trailing-whitespace t)
+  (paredit-mode 1)
+  (eldoc-mode)
+  (elisp-slime-nav-mode t)
+  (define-key emacs-lisp-mode-map (kbd "C-m") 'newline-and-indent))
 
-(add-hook 'ielm-mode-hook
-  (lambda ()
-    (paredit-mode 1)
-    (eldoc-mode)
-    (elisp-slime-nav-mode t)
-    (local-set-key (kbd "C-S-d") (lambda ()
-                                   (interactive)
-                                   (comint-send-eof)
-                                   (kill-buffer)))))
+(add-hook 'emacs-lisp-mode-hook #'/emacs-lisp-mode-hook)
+
+
+(defun /lisp-interaction-mode-hook ()
+  (paredit-mode 1)
+  (define-key lisp-interaction-mode-map (kbd "C-x C-j") 'eval-print-last-sexp)
+  (eldoc-mode))
+
+(add-hook 'lisp-interaction-mode-hook #'/lisp-interaction-mode-hook)
+
+
+(defun /ielm-mode-hook ()
+  (paredit-mode 1)
+  (eldoc-mode)
+  (elisp-slime-nav-mode t)
+  (local-set-key (kbd "C-S-d") (lambda ()
+                                 (interactive)
+                                 (comint-send-eof)
+                                 (kill-buffer))))
+
+(add-hook 'ielm-mode-hook #'/ielm-mode-hook)
