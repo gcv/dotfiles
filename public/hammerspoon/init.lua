1c509cfc9719319b5f596867a313fde8384243a6
Constantine Vetoshev <vetoshev@gmail.com>
2019-01-29 20:50:34 -0800

    Add a command to stop iTunes playback at the end of the current track.

--- public/hammerspoon/init.lua
+++ public/hammerspoon/init.lua
@@ -52,6 +52,38 @@ hs.hotkey.bind(
 )
 
 
+-- iTunes
+hs.hotkey.bind(
+   {"ctrl", "alt", "cmd"}, "p",
+   function()
+      if (hs.itunes.isRunning() and hs.itunes.isPlaying()) then
+         hs.alert("Will pause iTunes at the end of the current track.")
+         local trackNameStart = hs.itunes.getCurrentTrack()
+         local status = "default"
+         hs.timer.waitWhile(
+            function()
+               if ((not hs.itunes.isRunning()) or (not hs.itunes.isPlaying())) then
+                  status = "itunes-changed"
+                  return false
+               elseif (trackNameStart ~= hs.itunes.getCurrentTrack()) then
+                  status = "track-changed"
+                  return false
+               else
+                  return true
+               end
+            end,
+            function()
+               if (hs.itunes.isRunning() and hs.itunes.isPlaying() and status == "track-changed") then
+                  hs.itunes.pause()
+               end
+            end,
+            0.5
+         )
+      end
+   end
+)
+
+
 -- helpers
 
 function appSwitchEmacs()
