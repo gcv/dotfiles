cb17f8bea6c008af9f6b5f1cdeb04cf938ec0928
Constantine Vetoshev <vetoshev@gmail.com>
2019-04-24 16:43:46 -0700

    MELPA updates.

--- emacs/elpa/disk-usage-1.3.1/disk-usage-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "disk-usage" "1.3.1" "Sort and browse disk usage listings" '((emacs "26.1")) :url "https://gitlab.com/Ambrevar/emacs-disk-usage" :keywords '("files" "convenience" "tools"))
--- emacs/elpa/swiper-20190403.1424/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20190403.1424" "Isearch with an overview. Oh, man!" '((emacs "24.1") (ivy "0.11.0")) :commit "adad2e4b7231c448b3db24b2f13cd3e71bd55baa" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
