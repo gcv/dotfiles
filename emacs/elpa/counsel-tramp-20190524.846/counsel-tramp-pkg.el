c23bdd2a6dc23d66bc12a872ee715ee7d72a16a1
Constantine Vetoshev <vetoshev@gmail.com>
2019-05-24 16:03:48 -0700

    Install counsel-tramp.

---
+++ emacs/elpa/counsel-tramp-20190524.846/counsel-tramp-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-tramp" "20190524.846" "Tramp ivy interface for ssh, docker, vagrant" '((emacs "24.3") (counsel "0.10")) :commit "5ed06027a313b27ab17a8c8fabbc30c0e424c3b2" :authors '(("Masashı Mıyaura")) :maintainer '("Masashı Mıyaura") :url "https://github.com/masasam/emacs-counsel-tramp")
