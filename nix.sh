77269e172031b57ff28fe286de4890463572bf3b
Constantine Vetoshev <vetoshev@gmail.com>
2019-07-30 18:11:42 -0700

    Add Nix initial system installation script.

---
+++ nix.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+
+NIX_HOME=/nix
+
+if [[ ! -d "${NIX_HOME}" ]]; then
+    sudo mkdir -m 0755 /nix && chown `whoami` /nix
+    sh <(curl https://nixos.org/nix/install) --no-daemon
+fi
+
+
+# Recent macOS releases provide reasonable versions of:
+# - bash
+# - zsh
+# - git
+# - sqlite
+nix-env -i \
+    aspell \
+    autoconf \
+    autojump \
+    automake \
+    awscli \
+    cmake \
+    coreutils \
+    ctags \
+    direnv \
+    exif \
+    fdupes \
+    ffmpeg \
+    findutils \
+    fzf \
+    git \
+    global \
+    gnupg-1.4.23 \
+    htop \
+    jq \
+    ledger \
+    mosh \
+    nmap \
+    offlineimap \
+    p7zip \
+    pv \
+    rlwrap \
+    silver-searcher \
+    tectonic \
+    texinfo \
+    tmux \
+    tree \
+    watch \
+    wget \
+    xz \
+    youtube-dl
