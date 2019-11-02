541d86be35ba24b91235550072f04e0c773dfd6f
Constantine Vetoshev <vetoshev@gmail.com>
2019-11-02 07:49:13 -0700

    Improve Dark Mode toggle to set wallpaper.

--- public/hammerspoon/init.lua
+++ public/hammerspoon/init.lua
@@ -388,10 +388,21 @@ hs.hotkey.bind(
    {"ctrl", "alt", "cmd"}, "d",
    function()
       hs.osascript.applescript(
-         [[tell application "System Events"
+         [[set isdark to false
+           tell application "System Events"
              tell appearance preferences
                set dark mode to not dark mode
+               set isdark to (get dark mode)
              end tell
+             if isdark then
+               tell every desktop
+                 set picture to "/Library/Desktop Pictures/Solid Colors/Black.png"
+               end tell
+             else
+               tell every desktop
+                 set picture to "/Library/Desktop Pictures/Aqua Graphite.jpg"
+               end tell
+             end if
            end tell]]
       )
    end
