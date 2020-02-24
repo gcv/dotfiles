94d0036f80fdd021b905ab96a02525ae2a87c8d0
Constantine Vetoshev <vetoshev@gmail.com>
2020-02-24 05:12:24 -0800

    Put Ivy configuration back in packages.el with better lazy loading.

--- emacs/packages.el
+++ emacs/packages.el
@@ -161,13 +161,28 @@
 
 
 (use-package counsel                    ; Ivy / Swiper / Counsel
-  :pin melpa)
+  :pin melpa
+  :bind (("M-x" . counsel-M-x)
+         ("C-x C-M-f" . counsel-find-file)
+         ;;("C-M-y" . counsel-yank-pop)
+         ("M-i" . counsel-imenu))
+  :config (progn
+
+            (add-to-list 'ivy-re-builders-alist '(counsel-M-x . ivy--regex-fuzzy))
+
+            (add-to-list 'ivy-display-functions-alist '(counsel-M-x . /ivy-display-function-window))
+            (add-to-list 'ivy-display-functions-alist '(counsel-yank-pop . /ivy-display-function-window))
+            (add-to-list 'ivy-display-functions-alist '(counsel-ag . /ivy-display-function-window))
+
+            ))
 
 
 (use-package counsel-projectile
   :pin melpa
+  :defer t
   :config (progn
-            ;;(counsel-projectile-mode -1)
+            ;; To turn on, change :defer to nil.
+            ;;(counsel-projectile-mode 1)
             ))
 
 
@@ -552,7 +567,78 @@
 
 
 (use-package ivy                        ; Ivy / Swiper / Counsel
-  :pin melpa)
+  :pin melpa
+  :bind (("C-c c r" . ivy-resume)
+         :map ivy-minibuffer-map
+         ("C-m" . ivy-alt-done)         ; enter navigates into a directory
+         )
+  :config (progn
+
+            (setq ivy-ignore-buffers ignore-buffers)
+
+            ;;(setq ivy-initial-inputs-alist nil)
+
+            (setq ivy-posframe-width 65
+                  ivy-posframe-min-width 65
+                  ivy-posframe-height nil
+                  ivy-posframe-border-width 1
+                  ivy-posframe-parameters '((left-fringe . 0)
+                                            (right-fringe . 0)))
+
+            ;; Automatic Ivy window sizing with ivy-posframe awareness.
+            (defun /ivy-height-smart ()
+              (if (display-graphic-p)
+                  (if (eq '/ivy-display-function-window ivy--display-function)
+                      (window-height)
+                    (if (> (frame-height) 55)
+                        (round (/ (frame-height) 2.5))
+                      (round (/ (frame-height) 1.75))))
+                ;; non-graphic display
+                10))
+
+
+            ;; Full-window Ivy selection (requires /ivy-height-smart).
+            (defun /ivy-display-function-window (text)
+              (let ((buffer (get-buffer-create "*ivy-candidates*"))
+                    (str (with-current-buffer (get-buffer-create " *Minibuf-1*")
+                           (let ((point (point))
+                                 (string (concat (buffer-string) "  " text)))
+                             (ivy-add-face-text-property (- point 1) point 'ivy-cursor string t)
+                             string))))
+                (with-current-buffer buffer
+                  (let ((inhibit-read-only t))
+                    (erase-buffer)
+                    (insert str)
+                    (goto-char (point-min))
+                    (setq-local cursor-type nil)))
+                (with-ivy-window
+                  (display-buffer
+                   buffer
+                   `((display-buffer-reuse-window
+                      display-buffer-below-selected)
+                     (window-height . ,(ivy--height (ivy-state-caller ivy-last))))))))
+
+            (defun /ivy-display-function-window-cleanup ()
+              (when-let ((ivy-candidate-buf (get-buffer "*ivy-candidates*")))
+                (kill-buffer ivy-candidate-buf)))
+
+            (add-hook 'minibuffer-exit-hook #'/ivy-display-function-window-cleanup)
+
+            (setq ivy-height-alist
+                  '((swiper . 10)
+                    (t . (lambda (_caller)
+                           ;; XXX: This needs to match /ivy-posframe-display-smart.
+                           (/ivy-height-smart)))))
+
+            (setq ivy-re-builders-alist
+                  '((swiper . ivy--regex-plus)
+                    (t . ivy--regex-plus)))
+
+            (setq ivy-display-functions-alist
+                  '((ivy-completion-in-region . ivy-display-function-overlay)
+                    (t . nil)))
+
+            ))
 
 
 (use-package ivy-xref
@@ -561,16 +647,40 @@
   :init (progn
           (when (>= emacs-major-version 27)
             (setq xref-show-definitions-function #'ivy-xref-show-defs))
-          (setq xref-show-xrefs-function #'ivy-xref-show-xrefs))
-  :config (progn
-            ))
+          (setq xref-show-xrefs-function #'ivy-xref-show-xrefs)))
 
 
 (use-package ivy-posframe
   :pin melpa
+  :defer nil                            ; must load eagerly
   :diminish ""
   :config (progn
+
             (ivy-posframe-mode 1)
+
+            (set-face-attribute 'ivy-posframe-cursor nil :inherit 'ivy-cursor)
+            (set-face-attribute 'ivy-posframe nil :foreground nil :background nil :inherit 'default)
+
+            ;; Automatically place the posframe centered in the current window
+            ;; if large enough, or in the frame as a fallback.
+            (defun /ivy-posframe-display-smart (str)
+              (if (or (< (window-total-width (with-ivy-window (selected-window)))
+                         ivy-posframe-width)
+                      (< (window-total-height (with-ivy-window (selected-window)))
+                         ;; XXX: This needs to match ivy-height-alist.
+                         (/ivy-height-smart)))
+                  (ivy-posframe-display-at-frame-center str)
+                (ivy-posframe-display-at-window-center str)))
+
+            (setq ivy-posframe-display-functions-alist
+                  '((swiper . ivy-posframe-display-at-window-bottom-left)
+                    ;;(swiper . nil)
+                    (complete-symbol . ivy-posframe-display-at-point)
+                    (counsel-M-x . /ivy-posframe-display-smart)
+                    (counsel-yank-pop . /ivy-display-function-window)
+                    ;;(t . ivy-posframe-display)
+                    (t . /ivy-posframe-display-smart)))
+
             ))
 
 
@@ -854,6 +964,8 @@
 (use-package perspective
   ;;:load-path "~/Code/perspective-el"
   :pin melpa
+  :defer nil
+  :bind (("C-x C-M-b" . persp-ivy-switch-buffer))
   :config (progn
 
             (persp-mode)
@@ -1054,7 +1166,7 @@
 
 (use-package swiper                     ; Ivy / Swiper / Counsel
   :pin melpa
-  :bind ("M-S-C-s" . 'swiper))
+  :bind ("M-S-C-s" . swiper))
 
 
 (use-package terraform-mode
