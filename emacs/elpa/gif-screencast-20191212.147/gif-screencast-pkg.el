7922baae082c1d3703039d4228d49bdb13f4fe1a
Constantine Vetoshev <vetoshev@gmail.com>
2020-04-02 17:51:51 -0700

    ELPA upgrade: gif-screencast.

--- emacs/elpa/gif-screencast-20191212.147/gif-screencast-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "gif-screencast" "20191212.147" "One-frame-per-action GIF recording" '((emacs "25.1")) :commit "2ed2ebe710690f041c074cc59816ecd3e0f8e980" :keywords '("multimedia" "screencast") :authors '(("Pierre Neidhardt" . "mail@ambrevar.xyz")) :maintainer '("Pierre Neidhardt" . "mail@ambrevar.xyz") :url "https://gitlab.com/ambrevar/emacs-gif-screencast")
---
+++ emacs/elpa/gif-screencast-20200327.1332/gif-screencast-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "gif-screencast" "20200327.1332" "One-frame-per-action GIF recording" '((emacs "25.1")) :commit "e39786458fb30e2e9683094c75c6c2cef537d9c4" :keywords '("multimedia" "screencast") :authors '(("Pierre Neidhardt" . "mail@ambrevar.xyz")) :maintainer '("Pierre Neidhardt" . "mail@ambrevar.xyz") :url "https://gitlab.com/ambrevar/emacs-gif-screencast")
