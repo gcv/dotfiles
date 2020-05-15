88ae155a9244db6df4e3c1510577804b9e89894e
Constantine Vetoshev <vetoshev@gmail.com>
2020-05-15 12:52:02 -0700

    ELPA upgrade: web-mode.

--- emacs/elpa/web-mode-16/web-mode-autoloads.el
+++
@@ -1,28 +0,0 @@
-;;; web-mode-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil "web-mode" "../../../../../.emacs.d/elpa/web-mode-16/web-mode.el"
-;;;;;;  "23b2f86a323d888758f62ebade33de0f")
-;;; Generated autoloads from ../../../../../.emacs.d/elpa/web-mode-16/web-mode.el
-
-(autoload 'web-mode "web-mode" "\
-Major mode for editing web templates.
-
-\(fn)" t nil)
-
-;;;***
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/web-mode-16/web-mode-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/web-mode-16/web-mode.el") (23508
-;;;;;;  57697 698467 68000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; web-mode-autoloads.el ends here
