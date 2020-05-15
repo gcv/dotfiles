b0894a5975182fa5d54d3badc73f1c568a21187d
Constantine Vetoshev <vetoshev@gmail.com>
2020-05-15 12:57:10 -0700

    ELPA upgrade: olivetti.

--- emacs/elpa/olivetti-1.9.3/olivetti-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "olivetti" "1.9.3" "Minor mode for a nice writing environment" '((emacs "24.5")) :commit "67e32a7754cda4c8d94227e80bfa708abb4e8e6d" :keywords '("wp" "text") :authors '(("William Rankin" . "code@william.bydasein.com")) :maintainer '("William Rankin" . "code@william.bydasein.com") :url "https://gthub.com/rnkn/olivetti")
