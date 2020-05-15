c8604dde62390cafe54992cef05aaba287c36455
Constantine Vetoshev <vetoshev@gmail.com>
2020-05-15 12:51:44 -0700

    ELPA upgrade: counsel-projectile.

---
+++ emacs/elpa/counsel-projectile-20200430.2133/counsel-projectile-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "counsel-projectile" "20200430.2133" "Ivy integration for Projectile" '((counsel "0.13.0") (projectile "2.0.0")) :commit "126e825bbab872b3befd9ef88660571391ebfdc3" :keywords '("project" "convenience") :authors '(("Eric Danan")) :maintainer '("Eric Danan") :url "https://github.com/ericdanan/counsel-projectile")
