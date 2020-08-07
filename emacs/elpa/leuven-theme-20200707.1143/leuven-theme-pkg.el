6418a50398d90efd00642e85de8e64eec31bf9d8
Constantine Vetoshev <vetoshev@gmail.com>
2020-08-06 23:20:23 -0700

    ELPA upgrade: leuven-theme.

--- emacs/elpa/leuven-theme-20200707.1143/leuven-theme-pkg.el
+++ emacs/elpa/leuven-theme-20200707.1143/leuven-theme-pkg.el
@@ -1,4 +1,4 @@
-(define-package "leuven-theme" "20200513.1742" "Awesome Emacs color theme on white background" 'nil :keywords
+(define-package "leuven-theme" "20200707.1143" "Awesome Emacs color theme on white background" 'nil :commit "a116202c82613c087440d2e8a6a504b08f1862ce" :keywords
   '("color" "theme")
   :authors
   '(("Fabrice Niessen <(concat \"fniessen\" at-sign \"pirilampo.org\")>"))
