53a49bb82c7147bfda13ed412bde7adbae66153d
Constantine Vetoshev <vetoshev@gmail.com>
2020-08-28 11:45:32 -0700

    ELPA upgrade: vterm.

--- emacs/elpa/vterm-20200824.1318/vterm-pkg.el
+++ emacs/elpa/vterm-20200824.1318/vterm-pkg.el
@@ -1,6 +1,6 @@
-(define-package "vterm" "20200713.1344" "Fully-featured terminal emulator"
+(define-package "vterm" "20200824.1318" "Fully-featured terminal emulator"
   '((emacs "25.1"))
-  :commit "f41849c2c9c1899f22d1c3d4f871ec47c82627ce" :keywords
+  :commit "1348404fe9a78b1f1dd810f22892863e124123da" :keywords
   '("terminals")
   :authors
   '(("Lukas Fürmetz" . "fuermetz@mailbox.org"))
