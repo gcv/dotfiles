fb20b5454e56a7f80a851364044d4d2e5d470af0
Constantine Vetoshev <vetoshev@gmail.com>
2020-10-22 17:22:29 -0700

    Still hacking on Selectrum.

---
+++ emacs/elpa/maple-minibuffer-20200304.1841/maple-minibuffer-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/kostya/Code/dotfiles/emacs/elpa/maple-minibuffer-20200304.1841/maple-minibuffer.el  -*- no-byte-compile: t -*-
+(define-package "maple-minibuffer" "20200304.1841" "Initialize file configurations." 'nil :authors '(("lin.jiang" . "mail@honmaple.com")) :maintainer '("lin.jiang" . "mail@honmaple.com") :url "https://github.com/honmaple/emacs-maple-minibuffer")
--- emacs/selectrum.el
+++ emacs/selectrum.el
@@ -1,3 +1,111 @@
+;;; XXX: This is broken in four ways right now:
+;;;
+;;; - find-file wrapper splits (!) the current window when opening a file
+;;; - find-file has inconsistent behavior when opening a symlink to a
+;;;   vc-controlled file (sometimes prompts, sometimes not, sometimes errors,
+;;;   sometimes prompts in minibuffer, sometimes in mini frame)
+;;; - imenu-cr fails with an error
+;;; - imeni-cr should use an overlay or something to keep its history clean of "Function: " prefixes
+;;;
+;;; This breakage is reproducible with mini-frame and maple-minibuffer
+
+;; no mini-frame:
+;; (defun /with-selectrum (fn)
+;;   (let ((status-selectrum selectrum-mode))
+;;     (unwind-protect
+;;         (progn
+;;           (selectrum-mode 1)
+;;           (call-interactively fn))
+;;       (unless status-selectrum (selectrum-mode -1)))))
+
+(defun /with-selectrum (fn)
+  (let ((status-selectrum selectrum-mode))
+    (unwind-protect
+        (progn
+          (selectrum-mode 1)
+          (if window-system
+              (mini-frame-read-from-minibuffer (lambda () (call-interactively fn)))
+              ;; (maple-minibuffer:with (call-interactively fn))
+            (call-interactively fn)))
+      (unless status-selectrum (selectrum-mode -1)))))
+
+(defun /selectrum-M-x ()
+  (interactive)
+  (/with-selectrum #'execute-extended-command))
+
+(defun /selectrum-find-file ()
+  (interactive)
+  (/with-selectrum #'find-file))
+
+(defun /selectrum-imenu ()
+  (interactive)
+  (/with-selectrum #'imenu-cr))
+
+
+(use-package selectrum
+  :pin melpa
+  :defer t
+
+  :bind
+  (;;("M-x" . /selectrum-M-x)
+   ;;("C-x C-M-f" . /selectrum-find-file)
+   ;;("M-i" . /selectrum-imenu)
+   )
+
+  :config (progn
+
+            (when window-system
+              ;; match mini-frame height
+              (setq selectrum-num-candidates-displayed 15))
+
+            ;;(selectrum-mode 1)
+            (selectrum-prescient-mode 1)
+
+            ))
+
+
+(use-package selectrum-prescient
+  :commands (selectrum-prescient-mode))
+
+
+(use-package maple-minibuffer
+  :quelpa (maple-minibuffer :fetcher github :repo "honmaple/emacs-maple-minibuffer")
+
+  :if window-system
+
+  :config
+  (setq maple-minibuffer:position-type 'frame-center
+        maple-minibuffer:border-color "gray50"
+        maple-minibuffer:height nil
+        maple-minibuffer:width 0.7
+        maple-minibuffer:cache t)
+
+  (setq maple-minibuffer:action (list)
+        maple-minibuffer:ignore-action (list))
+
+  (add-to-list 'maple-minibuffer:action 'read-from-minibuffer)
+  (add-to-list 'maple-minibuffer:action 'read-string)
+
+  (add-to-list 'maple-minibuffer:ignore-action 'org-schedule)
+  (add-to-list 'maple-minibuffer:ignore-action 'find-alternate-file)
+  (add-to-list 'maple-minibuffer:ignore-action 'eval-expression)
+  (add-to-list 'maple-minibuffer:ignore-action 'edebug-eval-expression)
+  (add-to-list 'maple-minibuffer:ignore-action 'debugger-eval-expression)
+  (add-to-list 'maple-minibuffer:ignore-action 'find-alternate-file)
+
+  (add-to-list 'maple-minibuffer:ignore-regexp "^helm-")
+  (add-to-list 'maple-minibuffer:ignore-regexp "^magit-")
+
+  ;; more custom parameters for frame
+  (defun maple-minibuffer:parameters ()
+    "Maple minibuffer parameters."
+    `((height . ,(or maple-minibuffer:height 10))
+      (width . ,(or maple-minibuffer:width (window-pixel-width)))
+      (left-fringe . 5)
+      (right-fringe . 5)))
+  )
+
+
 (use-package mini-frame
   :pin melpa
   :if window-system
@@ -13,49 +121,6 @@
   (mini-frame-resize-max-height 15)
 
   :commands (mini-frame-read-from-minibuffer)
-
-  :bind
-  (;;("M-x" . /smf-M-x)
-   ;;("C-x C-M-f" . /smf-find-file)
-   ;;("M-i" . /smf-imenu)
-   )
-
-  :init
-  ;; This implementation effectively enables mini-frame for the command, without
-  ;; enabling the full mode. Reason: disabling the mode deletes the temporary
-  ;; mini-frame frame, which makes creating it again unnecessarily slow.
-  ;;
-  ;; Unfortunately, it has some subtle breakage when opening files has an
-  ;; additional prompt. For example, opening a symlink vc-controlled file should
-  ;; prompt to open the link target. This seems to work fine with full
-  ;; mini-frame mode enabled, but not with this hack.
-
-  (defun /with-mf-selectrum (fn)
-    (let ((status-selectrum selectrum-mode))
-      (unwind-protect
-          (progn
-            (selectrum-mode 1)
-            (advice-add 'read-from-minibuffer :around #'mini-frame-read-from-minibuffer)
-            (advice-add 'read-string :around #'mini-frame-read-from-minibuffer)
-            (advice-add 'minibuffer-selected-window :around #'mini-frame--minibuffer-selected-window)
-            (call-interactively fn))
-        (advice-remove 'read-from-minibuffer #'mini-frame-read-from-minibuffer)
-        (advice-remove 'read-string #'mini-frame-read-from-minibuffer)
-        (advice-remove 'minibuffer-selected-window #'mini-frame--minibuffer-selected-window)
-        (unless status-selectrum (selectrum-mode -1)))))
-
-  (defun /smf-M-x ()
-    (interactive)
-    (/with-mf-selectrum #'execute-extended-command))
-
-  (defun /smf-find-file ()
-    (interactive)
-    (/with-mf-selectrum #'find-file))
-
-  (defun /smf-imenu ()
-    (interactive)
-    (/with-mf-selectrum #'imenu-cr))
-
 
   :config
   ;; XXX: mini-frame-resize set to t does not quite work:
@@ -73,8 +138,6 @@
   ;;         (completion-in-region-function #'selectrum-completion-in-region))
   ;;     (mini-frame-read-from-minibuffer
   ;;      (lambda () (call-interactively #'execute-extended-command)))))
-  ;;
-  ;; Clean behavior for #'find-file requires a full mode flip:
 
   ;; (when window-system
   ;;   (mini-frame-mode 1))
@@ -84,22 +147,3 @@
   (add-to-list 'mini-frame-ignore-commands "helm-.*")
   (add-to-list 'mini-frame-ignore-commands "magit-.*")
   )
-
-
-(use-package selectrum
-  :pin melpa
-  :defer t
-  :config (progn
-
-            (when window-system
-              ;; match mini-frame height
-              (setq selectrum-num-candidates-displayed 15))
-
-            ;;(selectrum-mode 1)
-            (selectrum-prescient-mode 1)
-
-            ))
-
-
-(use-package selectrum-prescient
-  :commands (selectrum-prescient-mode))
