e1c6cc20f63ef07b20b9e4aebddbd66bdfe9c8d8
Constantine Vetoshev <vetoshev@gmail.com>
2021-01-01 22:23:32 -0800

    Emacs package update: swiper

--- emacs/elpa/swiper-20200503.1102/swiper-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "swiper" "20200503.1102" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :commit "04ca16420053a3a6d34a96f0d680dd449c2e5851" :keywords '("matching") :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :url "https://github.com/abo-abo/swiper")
---
+++ emacs/elpa/swiper-20201208.1419/swiper-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from /Users/kostya/Code/dotfiles/emacs/elpa/swiper-20201208.1419/swiper.el  -*- no-byte-compile: t -*-
+(define-package "swiper" "20201208.1419" "Isearch with an overview. Oh, man!" '((emacs "24.5") (ivy "0.13.0")) :commit "d2891aab7b816aebf21ebd01ce33933a6ac6244f" :authors '(("Oleh Krehel" . "ohwoeowho@gmail.com")) :maintainer '("Oleh Krehel" . "ohwoeowho@gmail.com") :keywords '("matching") :url "https://github.com/abo-abo/swiper")
