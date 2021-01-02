644c9cbc8f7dbb30767b014257bcf5350a77d760
Constantine Vetoshev <vetoshev@gmail.com>
2021-01-01 22:23:55 -0800

    Emacs package update: seq

--- emacs/elpa/seq-2.20/seq-autoloads.el
+++
@@ -1,18 +0,0 @@
-;;; seq-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/seq-2.20/seq-24.el"
-;;;;;;  "../../../../../.emacs.d/elpa/seq-2.20/seq-25.el" "../../../../../.emacs.d/elpa/seq-2.20/seq-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/seq-2.20/seq-pkg.el" "../../../../../.emacs.d/elpa/seq-2.20/seq.el")
-;;;;;;  (23508 57455 546858 545000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; seq-autoloads.el ends here
