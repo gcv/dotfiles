2190f7ded6daf1013501e9f342d0e650ed624e72
Constantine Vetoshev <vetoshev@gmail.com>
2021-01-01 22:21:25 -0800

    Emacs package update: ivy-posframe

--- emacs/elpa/ivy-posframe-20201215.39/ivy-posframe-autoloads.el
+++ emacs/elpa/ivy-posframe-20201215.39/ivy-posframe-autoloads.el
@@ -22,12 +22,12 @@ or call the function `ivy-posframe-mode'.")
 (autoload 'ivy-posframe-mode "ivy-posframe" "\
 Display ivy via posframe.
 
+If called interactively, enable Ivy-Posframe mode if ARG is
+positive, and disable it if ARG is zero or negative.  If called
+from Lisp, also enable the mode if ARG is omitted or nil, and
+toggle it if ARG is `toggle'; disable the mode otherwise.
+
 \(fn &optional ARG)" t nil)
-
-(autoload 'ivy-posframe-enable "ivy-posframe" "\
-
-
-\(fn)" t nil)
 
 (if (fboundp 'register-definition-prefixes) (register-definition-prefixes "ivy-posframe" '("ivy-posframe-")))
 
