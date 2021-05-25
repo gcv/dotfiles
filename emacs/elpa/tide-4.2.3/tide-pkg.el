f53d66da5e967b4d7fc1bc467016c8b3957b99c8
Constantine Vetoshev <vetoshev@gmail.com>
2021-05-25 15:49:48 -0700

    Emacs package update: tide

---
+++ emacs/elpa/tide-4.2.3/tide-pkg.el
@@ -0,0 +1,17 @@
+(define-package "tide" "4.2.3" "Typescript Interactive Development Environment"
+  '((emacs "25.1")
+    (dash "2.10.0")
+    (s "1.11.0")
+    (flycheck "27")
+    (typescript-mode "0.1")
+    (cl-lib "0.5"))
+  :commit "2a3ac4f38472d66e2d8a6bbe5dadb52bc008acbd" :authors
+  '(("Anantha kumaran" . "ananthakumaran@gmail.com"))
+  :maintainer
+  '("Anantha kumaran" . "ananthakumaran@gmail.com")
+  :keywords
+  '("typescript")
+  :url "http://github.com/ananthakumaran/tide")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
