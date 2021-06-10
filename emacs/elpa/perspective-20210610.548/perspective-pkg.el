b37f8f92994ab86915bf9a407b9faa601fc52030
Constantine Vetoshev <vetoshev@gmail.com>
2021-06-10 16:39:55 -0700

    ELPA update: perspective.

---
+++ emacs/elpa/perspective-20210610.548/perspective-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from perspective.el  -*- no-byte-compile: t -*-
+(define-package "perspective" "20210610.548" "switch between named \"perspectives\" of the editor" '((emacs "24.4") (cl-lib "0.5")) :commit "68b13cef8435cae581850415cdda17e245dd0eae" :authors '(("Natalie Weizenbaum" . "nex342@gmail.com")) :maintainer '("Natalie Weizenbaum" . "nex342@gmail.com") :keywords '("workspace" "convenience" "frames") :url "http://github.com/nex3/perspective-el")
