cd928d4a79e3aa9fc7501aaa76a79e37f0598367
Constantine Vetoshev <vetoshev@gmail.com>
2021-07-07 17:14:19 -0700

    Emacs package update: swift-mode

--- emacs/elpa/swift-mode-20210613.543/swift-mode-pkg.el
+++ emacs/elpa/swift-mode-20210613.543/swift-mode-pkg.el
@@ -1,7 +1,7 @@
-(define-package "swift-mode" "20210518.728" "Major-mode for Apple's Swift programming language"
+(define-package "swift-mode" "20210613.543" "Major-mode for Apple's Swift programming language"
   '((emacs "24.4")
     (seq "2.3"))
-  :commit "4a5f57722e06dad3bbbb1a496ef7d1b732ac7c46" :keywords
+  :commit "1b47a09f1c0e15c543e0551e7f1e643f437e7711" :keywords
   '("languages" "swift")
   :url "https://github.com/swift-emacs/swift-mode")
 ;; Local Variables:
