6de4d1d0fafe56c8d23bbfd1632daa3b8a6b3fce
Constantine Vetoshev <vetoshev@gmail.com>
2021-07-07 17:14:10 -0700

    Emacs package update: julia-snail

--- emacs/elpa/julia-snail-20210624.547/julia-snail-pkg.el
+++ emacs/elpa/julia-snail-20210624.547/julia-snail-pkg.el
@@ -1,11 +1,11 @@
-(define-package "julia-snail" "20210330.1901" "Julia Snail"
+(define-package "julia-snail" "20210624.547" "Julia Snail"
   '((emacs "26.2")
     (dash "2.16.0")
     (julia-mode "0.3")
     (s "1.12.0")
     (spinner "1.7.3")
     (vterm "0.0.1"))
-  :commit "03b4296ba7151963eb3c850f3314b02644101f51" :url "https://github.com/gcv/julia-snail")
+  :commit "47f1b125f8364a0aa8c072fdc7b4007aad5c0724" :url "https://github.com/gcv/julia-snail")
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
