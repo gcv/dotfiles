71431be22eb5e75ac7a0d69db4fa9eb837ed52f7
Constantine Vetoshev <vetoshev@gmail.com>
2021-08-13 21:03:01 -0700

    Emacs package update: elpher

---
+++ emacs/elpa/elpher-3.2.2/elpher-pkg.el
@@ -0,0 +1,12 @@
+(define-package "elpher" "3.2.2" "A friendly gopher and gemini client"
+  '((emacs "27.1"))
+  :commit "7b52709ddf798fe53db3855eef52ca8862e7700d" :authors
+  '(("Tim Vaughan" . "plugd@thelambdalab.xyz"))
+  :maintainer
+  '("Tim Vaughan" . "plugd@thelambdalab.xyz")
+  :keywords
+  '("comm" "gopher")
+  :url "https://thelambdalab.xyz/elpher")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
