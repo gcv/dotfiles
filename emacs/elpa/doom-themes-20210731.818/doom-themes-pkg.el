a71344f11ec18e456cb2786c4797814a9efbd4bc
Constantine Vetoshev <vetoshev@gmail.com>
2021-08-13 21:02:39 -0700

    Emacs package update: doom-themes

--- emacs/elpa/doom-themes-20210731.818/doom-themes-pkg.el
+++ emacs/elpa/doom-themes-20210731.818/doom-themes-pkg.el
@@ -1,7 +1,7 @@
-(define-package "doom-themes" "20210702.1109" "an opinionated pack of modern color-themes"
+(define-package "doom-themes" "20210731.818" "an opinionated pack of modern color-themes"
   '((emacs "25.1")
     (cl-lib "0.5"))
-  :commit "ac359fcc85b1bf8669f99263096affbcaab8e1a8" :authors
+  :commit "65fb964f36939cf412d03b3fe410618caf99c494" :authors
   '(("Henrik Lissner <https://github.com/hlissner>"))
   :maintainer
   '("Henrik Lissner" . "henrik@lissner.net")
