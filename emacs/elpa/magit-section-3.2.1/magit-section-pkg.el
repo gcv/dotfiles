df54039f015086e7d399aa77f3dc66540587bab3
Constantine Vetoshev <vetoshev@gmail.com>
2021-08-13 21:02:36 -0700

    Emacs package update: magit-section

---
+++ emacs/elpa/magit-section-3.2.1/magit-section-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from magit-section.el  -*- no-byte-compile: t -*-
+(define-package "magit-section" "3.2.1" "Sections for read-only buffers" '((emacs "25.1") (dash "2.18.1")) :keywords '("tools") :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :url "https://github.com/magit/magit")
