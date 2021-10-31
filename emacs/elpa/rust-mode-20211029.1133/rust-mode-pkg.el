2f4cf67f62f5d951e09da3bb4b43243964b0aef9
Constantine Vetoshev <vetoshev@gmail.com>
2021-10-31 08:35:00 -0700

    Emacs package update: rust-mode

--- emacs/elpa/rust-mode-20211029.1133/rust-mode-pkg.el
+++ emacs/elpa/rust-mode-20211029.1133/rust-mode-pkg.el
@@ -1,6 +1,6 @@
-(define-package "rust-mode" "20210423.1157" "A major-mode for editing Rust source code"
+(define-package "rust-mode" "20211029.1133" "A major-mode for editing Rust source code"
   '((emacs "25.1"))
-  :commit "494d59f92cbe12533eb89b202fc4f5342afcd543" :authors
+  :commit "3b81e81097463e7161de047ad340e4fe572dcc2a" :authors
   '(("Mozilla"))
   :maintainer
   '("Mozilla")
