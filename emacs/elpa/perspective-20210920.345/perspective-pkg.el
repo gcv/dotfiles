b536dc6d1b7562a605f1a1a48660dab54f9fb22f
gcv <gepardcv@gmail.com>
2021-11-26 12:43:13 -0800

    Emacs package update: perspective

--- emacs/elpa/perspective-20210920.345/perspective-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from perspective.el  -*- no-byte-compile: t -*-
-(define-package "perspective" "20210920.345" "switch between named \"perspectives\" of the editor" '((emacs "24.4") (cl-lib "0.5")) :commit "ca6f778a3f1995600fc3d369bc636888812c80cc" :authors '(("Natalie Weizenbaum" . "nex342@gmail.com")) :maintainer '("Natalie Weizenbaum" . "nex342@gmail.com") :keywords '("workspace" "convenience" "frames") :url "http://github.com/nex3/perspective-el")
