86a74a88e548e391d6e9dab451c3aac7343a65e9
gcv <gepardcv@gmail.com>
2021-12-31 01:37:40 -0800

    Update posframe ELPA package.

--- emacs/elpa/posframe-20211222.256/posframe-pkg.el
+++ emacs/elpa/posframe-20211222.256/posframe-pkg.el
@@ -1,6 +1,6 @@
-(define-package "posframe" "20211126.944" "Pop a posframe (just a frame) at point"
+(define-package "posframe" "20211222.256" "Pop a posframe (just a frame) at point"
   '((emacs "26.1"))
-  :commit "3b1dc400d286b0a4bd42e518bf3e7eedb49fd1e6" :authors
+  :commit "80cb98aff695a102772cc414d96611bdaf96f00e" :authors
   '(("Feng Shu" . "tumashu@163.com"))
   :maintainer
   '("Feng Shu" . "tumashu@163.com")
