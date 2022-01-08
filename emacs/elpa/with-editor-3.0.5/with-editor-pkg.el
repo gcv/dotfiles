2d75c9b8224976a6e3e1240fbe305a75d39b00f5
gcv <gepardcv@gmail.com>
2022-01-08 14:55:24 -0800

    Emacs package update: with-editor

--- emacs/elpa/with-editor-3.0.5/with-editor-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from with-editor.el  -*- no-byte-compile: t -*-
-(define-package "with-editor" "3.0.5" "Use the Emacsclient as $EDITOR" '((emacs "24.4")) :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :keywords '("tools") :url "https://github.com/magit/with-editor")
---
+++ emacs/elpa/with-editor-3.1.1/with-editor-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from with-editor.el  -*- no-byte-compile: t -*-
+(define-package "with-editor" "3.1.1" "Use the Emacsclient as $EDITOR" '((emacs "24.4")) :authors '(("Jonas Bernoulli" . "jonas@bernoul.li")) :maintainer '("Jonas Bernoulli" . "jonas@bernoul.li") :keywords '("tools") :url "https://github.com/magit/with-editor")
