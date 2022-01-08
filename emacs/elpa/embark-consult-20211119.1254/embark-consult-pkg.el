515aaabd312e79ae52ee4033000560b7bccffd03
gcv <gepardcv@gmail.com>
2022-01-08 14:55:15 -0800

    Emacs package update: embark-consult

--- emacs/elpa/embark-consult-20211119.1254/embark-consult-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from embark-consult.el  -*- no-byte-compile: t -*-
-(define-package "embark-consult" "20211119.1254" "Consult integration for Embark" '((emacs "26.1") (embark "0.12") (consult "0.10")) :commit "e0057f6a6621b24e0ba4c167b683a84039c6f084" :authors '(("Omar Antolín Camarena" . "omar@matem.unam.mx")) :maintainer '("Omar Antolín Camarena" . "omar@matem.unam.mx") :keywords '("convenience") :url "https://github.com/oantolin/embark")
---
+++ emacs/elpa/embark-consult-20211231.1502/embark-consult-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from embark-consult.el  -*- no-byte-compile: t -*-
+(define-package "embark-consult" "20211231.1502" "Consult integration for Embark" '((emacs "26.1") (embark "0.12") (consult "0.10")) :commit "8cf1fdbfacdbdb98ca3b4e50bf295059a02fe4a2" :authors '(("Omar Antolín Camarena" . "omar@matem.unam.mx")) :maintainer '("Omar Antolín Camarena" . "omar@matem.unam.mx") :keywords '("convenience") :url "https://github.com/oantolin/embark")
