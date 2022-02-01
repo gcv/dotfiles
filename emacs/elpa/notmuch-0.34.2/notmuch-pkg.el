c82207000bf4cfe2c23a74bceb0073f4fcb75725
gcv <gepardcv@gmail.com>
2022-02-01 14:11:36 -0800

    Emacs package update: notmuch

--- emacs/elpa/notmuch-0.34.2/notmuch-pkg.el
+++
@@ -1,4 +0,0 @@
-(define-package "notmuch" "0.34.2" "run notmuch within emacs" 'nil :commit "a254a15861d3510adbe2897fed100a3c77642165" :url "https://notmuchmail.org")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
