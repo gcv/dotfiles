fffcfced0a06cb6de07d49d3ea327b032abc2c44
gcv <gepardcv@gmail.com>
2022-03-29 23:17:47 -0700

    Emacs package update: undo-fu

--- emacs/elpa/undo-fu-20220211.548/undo-fu.el
+++ emacs/elpa/undo-fu-20220211.548/undo-fu.el
@@ -1,27 +1,15 @@
 ;;; undo-fu.el --- Undo helper with redo -*- lexical-binding: t -*-
 
+;; SPDX-License-Identifier: GPL-2.0-or-later
 ;; Copyright (C) 2019  Campbell Barton, 2020 Free Software Foundation, Inc.
 
 ;; Author: Campbell Barton <ideasman42@gmail.com>
 
 ;; URL: https://gitlab.com/ideasman42/emacs-undo-fu
-;; Package-Version: 20211030.612
-;; Package-Commit: ab8bc10e424bccc847800c31ab41888db789d55d
+;; Package-Version: 20220211.548
+;; Package-Commit: e81c8da4416b15cac9d5ac7574e11471417a65ca
 ;; Version: 0.4
 ;; Package-Requires: ((emacs "25.1"))
-
-;; This program is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation, either version 3 of the License, or
-;; (at your option) any later version.
-
-;; This program is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
