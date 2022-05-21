a990589fc602bb5e1bd4bc508bbb861009c864f9
gcv <gepardcv@gmail.com>
2022-05-21 15:37:53 -0700

    Emacs package update: orderless

--- emacs/elpa/orderless-20220418.2119/orderless-pkg.el
+++ emacs/elpa/orderless-20220418.2119/orderless-pkg.el
@@ -1,6 +1,6 @@
-(define-package "orderless" "20220406.1434" "Completion style for matching regexps in any order"
+(define-package "orderless" "20220418.2119" "Completion style for matching regexps in any order"
   '((emacs "26.1"))
-  :commit "8f64537f556f26492fe5ee401d8d578d7d88684b" :authors
+  :commit "75eeae21971d86b51a712ed8ecd6434463b2d866" :authors
   '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
   :maintainer
   '("Omar Antolín Camarena" . "omar@matem.unam.mx")
