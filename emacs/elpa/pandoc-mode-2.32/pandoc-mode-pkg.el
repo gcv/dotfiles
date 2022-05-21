bb4407803cb0f724a77d1daf5ff68da45e17108c
gcv <gepardcv@gmail.com>
2022-05-21 15:37:49 -0700

    Emacs package update: pandoc-mode

--- emacs/elpa/pandoc-mode-2.32/pandoc-mode-pkg.el
+++ emacs/elpa/pandoc-mode-2.32/pandoc-mode-pkg.el
@@ -1,7 +1,7 @@
-(define-package "pandoc-mode" "2.31" "Minor mode for interacting with Pandoc"
+(define-package "pandoc-mode" "2.32" "Minor mode for interacting with Pandoc"
   '((hydra "0.10.0")
     (dash "2.10.0"))
-  :commit "500e80666fb779457be8771c5613c177187ba0cc" :authors
+  :commit "2a4e726a29d38e7c2379787cad619e5392ad2da0" :authors
   '(("Joost Kremers" . "joostkremers@fastmail.fm"))
   :maintainer
   '("Joost Kremers" . "joostkremers@fastmail.fm")
