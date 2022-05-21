3a70616363127a75719519fe3296bbd7cef9552d
gcv <gepardcv@gmail.com>
2022-05-21 15:38:11 -0700

    Emacs package update: vterm

--- emacs/elpa/vterm-20220429.21/vterm-pkg.el
+++ emacs/elpa/vterm-20220429.21/vterm-pkg.el
@@ -1,6 +1,6 @@
-(define-package "vterm" "20211226.817" "Fully-featured terminal emulator"
+(define-package "vterm" "20220429.21" "Fully-featured terminal emulator"
   '((emacs "25.1"))
-  :commit "a940dd2ee8a82684860e320c0f6d5e15d31d916f" :authors
+  :commit "b44723552f86407d528c4a6c8057382c061b008e" :authors
   '(("Lukas Fürmetz" . "fuermetz@mailbox.org"))
   :maintainer
   '("Lukas Fürmetz" . "fuermetz@mailbox.org")
