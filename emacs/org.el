4074043612f081c2d66d31a0405a6ac63fc9799f
gcv <gepardcv@gmail.com>
2022-06-11 15:42:16 -0700

    Emacs 28 updates.

--- emacs/org.el
+++ emacs/org.el
@@ -12,7 +12,7 @@
   (require 'ob-calc)
   (require 'ob-emacs-lisp)
   (require 'ob-gnuplot)
-  (require 'ob-ledger)
+  (unless (version< emacs-version "28") (require 'ob-ledger))
   (require 'ob-lilypond)
   (require 'ob-lua)
   (require 'ob-octave)
