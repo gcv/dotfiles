1cbdf3bb8af71b35187e9c9c287192bf61738eae
gcv <gepardcv@gmail.com>
2022-06-15 21:26:18 -0700

    Improve fish configuration fzf load.

--- public/fish/config.fish
+++ public/fish/config.fish
@@ -6,6 +6,12 @@ if status is-interactive
    type -q autojump && source ~/.nix-profile/share/autojump/autojump.fish
    type -q direnv && direnv hook fish | source
    type -q starship && starship init fish | source
-   test -e ~/.nix-profile/share/fzf/key-bindings.fish && source ~/.nix-profile/share/fzf/key-bindings.fish && fzf_key_bindings
+   if test -e ~/.nix-profile/share/fzf/key-bindings.fish
+       source ~/.nix-profile/share/fzf/key-bindings.fish && fzf_key_bindings
+   else if test -e /usr/share/fzf/fzf-key-bindings.fish
+       source /usr/share/fzf/fzf-key-bindings.fish && fzf_key_bindings
+   else if test -e /usr/local/share/fzf/fzf-key-bindings.fish
+       source /usr/local/share/fzf/fzf-key-bindings.fish && fzf_key_bindings
+   end
 
 end
