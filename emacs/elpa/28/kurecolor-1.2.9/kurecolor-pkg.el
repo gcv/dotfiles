ebeae4ce84024c706673ab49238ce951e2798c83
gcv <gepardcv@gmail.com>
2022-06-15 20:38:19 -0700

    Add Emacs 28 packages.

---
+++ emacs/elpa/28/kurecolor-1.2.9/kurecolor-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from kurecolor.el  -*- no-byte-compile: t -*-
+(define-package "kurecolor" "1.2.9" "color editing goodies" '((emacs "24.1") (s "1.0")) :commit "076de3e420210fc479675389acb3fed6ada9c8e0" :authors '(("Jason Milkins" . "jasonm23@gmail.com")) :maintainer '("Jason Milkins" . "jasonm23@gmail.com"))
