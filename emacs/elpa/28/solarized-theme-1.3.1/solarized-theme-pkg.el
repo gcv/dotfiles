e32ce969fa7e89bf30cfce33261acca175e04a00
gcv <gepardcv@gmail.com>
2022-07-21 22:22:02 -0700

    Emacs package update: solarized-theme

--- emacs/elpa/28/solarized-theme-1.3.1/solarized-theme-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "solarized-theme" "1.3.1" "The Solarized color theme, ported to Emacs."
-  '((emacs "24.1")
-    (cl-lib "0.5")
-    (dash "2.6.0"))
-  :commit "55cd77b61b6968048c61e13358ba487d217f24c0")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
