6324dd99fe63f1f8d291409aa02adbabbcd4927a
gcv <gepardcv@gmail.com>
2022-07-21 22:11:00 -0700

    Emacs package update: helm-core

---
+++ emacs/elpa/27/helm-core-3.8.6/helm-core-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from helm-core.el  -*- no-byte-compile: t -*-
+(define-package "helm-core" "3.8.6" "Development files for Helm" '((emacs "25.1") (async "1.9.4")) :commit "0fc13ddb13b1c8e70eb6c0c7f7d46308a55f2344" :authors '(("Thierry Volpiatto ")) :maintainer '("Thierry Volpiatto ") :url "https://emacs-helm.github.io/helm/")
