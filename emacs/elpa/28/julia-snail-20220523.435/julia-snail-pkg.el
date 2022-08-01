86db6a9ce3e4ff90124feacbd17e7f15036c0b6c
gcv <gepardcv@gmail.com>
2022-08-01 14:16:46 -0700

    Emacs package update: julia-snail

---
+++ emacs/elpa/28/julia-snail-20220523.435/julia-snail-pkg.el
@@ -0,0 +1,12 @@
+(define-package "julia-snail" "20220523.435" "Julia Snail"
+  '((emacs "26.2")
+    (dash "2.16.0")
+    (julia-mode "0.3")
+    (s "1.12.0")
+    (spinner "1.7.3")
+    (vterm "0.0.1")
+    (popup "0.5.9"))
+  :commit "326da9a7d0463ed2727f66dcbd4cb58b517ffe25" :url "https://github.com/gcv/julia-snail")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
