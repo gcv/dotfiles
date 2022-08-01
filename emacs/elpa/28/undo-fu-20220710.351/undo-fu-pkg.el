de4e2160a06c0d006b542e611c2cd7fa4d7edd8b
gcv <gepardcv@gmail.com>
2022-08-01 14:43:22 -0700

    Emacs package update: undo-fu

--- emacs/elpa/28/undo-fu-20220710.351/undo-fu-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; Generated package description from undo-fu.el  -*- no-byte-compile: t -*-
-(define-package "undo-fu" "20220710.351" "Undo helper with redo" '((emacs "25.1")) :commit "913cbb4ff3659d8537c764e56953dbb1b85e73c9" :authors '(("Campbell Barton" . "ideasman42@gmail.com")) :maintainer '("Campbell Barton" . "ideasman42@gmail.com") :url "https://codeberg.org/ideasman42/emacs-undo-fu")
