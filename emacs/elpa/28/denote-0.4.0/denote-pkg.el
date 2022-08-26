3cc869ac5b47de0f8ed3c03dc3b800e3ed4e5082
gcv <gepardcv@gmail.com>
2022-08-25 21:15:53 -0700

    Emacs package update: denote

--- emacs/elpa/28/denote-0.4.0/denote-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from denote.el  -*- no-byte-compile: t -*-
-(define-package "denote" "0.4.0" "Simple notes with an efficient file-naming scheme" '((emacs "27.2")) :commit "0ca421e3d403e0e81882b1a3584f482b97d27a15" :authors '(("Protesilaos Stavrou" . "info@protesilaos.com")) :maintainer '("Denote Development" . "~protesilaos/denote@lists.sr.ht") :url "https://git.sr.ht/~protesilaos/denote")
--- emacs/elpa/28/denote-0.5.1/denote-autoloads.el
+++ emacs/elpa/28/denote-0.5.1/denote-autoloads.el
@@ -35,7 +35,11 @@ When called from Lisp, all arguments are optional.
   and time like 2022-06-16 14:30.  A nil value or an empty string
   is interpreted as the `current-time'.
 
-\(fn &optional TITLE KEYWORDS FILE-TYPE SUBDIRECTORY DATE)" t nil)
+- TEMPLATE is a symbol which represents the key of a cons cell in
+  the user option `denote-templates'.  The value of that key is
+  inserted to the newly created buffer after the front matter.
+
+\(fn &optional TITLE KEYWORDS FILE-TYPE SUBDIRECTORY DATE TEMPLATE)" t nil)
 
 (autoload 'denote-type "denote" "\
 Create note while prompting for a file type.
@@ -62,19 +66,24 @@ Create note while prompting for a subdirectory.
 Available candidates include the value of the variable
 `denote-directory' and any subdirectory thereof.
 
-This is equivalent to calling `denote' when `denote-prompts' is set to
-\\='(subdirectory title keywords)." t nil)
+This is equivalent to calling `denote' when `denote-prompts' is
+set to \\='(subdirectory title keywords)." t nil)
 
 (function-put 'denote-subdirectory 'interactive-only 't)
 
-(register-definition-prefixes "denote" '("denote-"))
+(autoload 'denote-template "denote" "\
+Create note while prompting for a template.
 
-;;;***
-
-;;;### (autoloads nil "denote-dired" "denote-dired.el" (0 0 0 0))
-;;; Generated autoloads from denote-dired.el
+Available candidates include the keys in the `denote-templates'
+alist.  The value of the selected key is inserted in the newly
+created note after the front matter.
 
-(autoload 'denote-dired-rename-file "denote-dired" "\
+This is equivalent to calling `denote' when `denote-prompts' is
+set to \\='(template title keywords)." t nil)
+
+(function-put 'denote-template 'interactive-only 't)
+
+(autoload 'denote-rename-file "denote" "\
 Rename file and update existing front matter if appropriate.
 
 If in Dired, consider FILE to be the one at point, else prompt
@@ -87,16 +96,15 @@ modification time.  If such attribute cannot be found, the
 identifier falls back to the `current-time'.
 
 The default TITLE is retrieved from a line starting with a title
-field in the file's contents, depending on the given file type.
-Else, the file name is used as a default value at the minibuffer
-prompt.
+field in the file's contents, depending on the given file
+type (e.g. #+title for Org).  Else, the file name is used as a
+default value at the minibuffer prompt.
 
 As a final step after the FILE, TITLE, and KEYWORDS prompts, ask
 for confirmation, showing the difference between old and new file
-names.  If `denote-dired-rename-expert' is non-nil, conduct the
-renaming operation outright---no question asked!
+names.
 
-The file type extension (e.g. .pdf) is read from the underlying
+The file type extension (like .txt) is read from the underlying
 file and is preserved through the renaming process.  Files that
 have no extension are simply left without one.
 
@@ -111,39 +119,29 @@ to double-check the effect).  The rewrite of the FILE and
 KEYWORDS in the front matter should not affect the rest of the
 block.
 
-If the file doesn't have front matter, skip this step (see the
-command `denote-dired-rename-file-and-add-front-matter').
+If the file doesn't have front matter but is among the supported
+file types (per `denote-file-type'), add front matter at the top
+of it and leave the buffer unsaved for further inspection.
 
-This command is intended to (i) rename existing Denote notes
-while updating their title and keywords in the front matter, (ii)
-rename files that can benefit from Denote's file-naming scheme.
-The latter is a convenience we provide, since we already have all
-the requisite mechanisms in place (though Denote does not---and
-will not---manage such files).
-
-\(fn FILE TITLE KEYWORDS)" t nil)
-
-(autoload 'denote-dired-rename-file-and-add-front-matter "denote-dired" "\
-Rename FILE and unconditionally add front matter.
-
-This command has the same modalities of interaction as
-`denote-dired-rename-file' in terms of the FILE, TITLE, and
-KEYWORDS prompts, except it always inserts front matter at the
-start of the file.  It does not check if any front matter is
-already present.
-
-Front matter is added only when the file is one of the supported
-file types (per `denote-file-type').  For per-file-type front
-matter, refer to the variables:
+For per-file-type front matter, refer to the variables:
 
 - `denote-org-front-matter'
 - `denote-text-front-matter'
 - `denote-toml-front-matter'
 - `denote-yaml-front-matter'
 
+This command is intended to (i) rename existing Denote notes
+while updating their title and keywords in the front matter, (ii)
+convert existing supported file types to Denote notes, and (ii)
+rename non-note files (e.g. PDF) that can benefit from Denote's
+file-naming scheme.  The latter is a convenience we provide,
+since we already have all the requisite mechanisms in
+place (though Denote does not---and will not---manage such
+files).
+
 \(fn FILE TITLE KEYWORDS)" t nil)
 
-(autoload 'denote-dired-rename-marked-files "denote-dired" "\
+(autoload 'denote-dired-rename-marked-files "denote" "\
 Rename marked files in Dired to Denote file name.
 
 The operation does the following:
@@ -160,31 +158,100 @@ The operation does the following:
 - a prompt is asked once for the KEYWORDS field and the input is
   applied to all file names;
 
-- if the file is recognized as a Denote note, rewrite its front
-  matter to include the new keywords.  A confirmation to carry
-  out this step is performed once at the outset.  Note that the
-  affected buffers are not saved.  The user can thus check them
-  to confirm that the new front matter does not cause any
+- if the file is recognized as a Denote note, add a front matter
+  or rewrite it to include the new keywords.  A confirmation to
+  carry out this step is performed once at the outset.  Note that
+  the affected buffers are not saved.  The user can thus check
+  them to confirm that the new front matter does not cause any
   problems (e.g. with the command `diff-buffer-with-file').
   Multiple buffers can be saved with `save-some-buffers' (read
-  its doc string)." '(dired-mode) nil)
+  its doc string).  The addition of front matter takes place only
+  if the given file has the appropriate file type extension (per
+  the user option `denote-file-type')." '(dired-mode) nil)
 
-(autoload 'denote-dired-rename-marked-files-and-add-front-matters "denote-dired" "\
-Like `denote-dired-rename-marked-files' but add front matter.
+(autoload 'denote-rename-file-using-front-matter "denote" "\
+Rename FILE using its front matter as input.
+When called interactively, FILE is the return value of the
+function `buffer-file-name' which is subsequently inspected for
+the requisite front matter.  It is thus implied that the FILE has
+a file type that is supported by Denote, per `denote-file-type'.
 
-The additon of front matter takes place only if the given file
-has the appropriate file type extension (per the user option
-`denote-file-type').
+Ask for confirmation, showing the difference between the old and
+the new file names.  Refrain from performing the operation if the
+buffer has unsaved changes.
 
-Buffers are not saved.  The user can thus check them to confirm
-that the new front matter does not cause any problems (e.g. by
-invoking the command `diff-buffer-with-file').
+Never modify the identifier of the FILE, if any, even if it is
+edited in the front matter.  Denote considers the file name to be
+the source of truth in this case to avoid potential breakage with
+typos and the like.
 
-Multiple buffers can be saved with `save-some-buffers' (read its
-doc string)." '(dired-mode) nil)
+\(fn FILE)" t nil)
 
-(autoload 'denote-dired-mode "denote-dired" "\
-Fontify all Denote-style file names in Dired.
+(autoload 'denote-dired-rename-marked-files-using-front-matter "denote" "\
+Rename marked files in Dired using their front matter as input.
+Marked files must count as notes for the purposes of Denote,
+which means that they at least have an identifier in their file
+name and use a supported file type, per `denote-file-type'.
+Files that do not meet this criterion are ignored.
+
+The operation does the following:
+
+- the title in the front matter becomes the TITLE component of
+  the file name, with hyphenation per Denote's file-naming
+  scheme;
+
+- the keywords in the front matter are used for the KEYWORDS
+  component of the file name and are processed accordingly, if
+  needed;
+
+- the identifier remains unchanged in the file name even if it is
+  modified in the front matter (this is done to avoid breakage
+  caused by typos and the like).
+
+NOTE that files must be saved, because Denote reads from the
+underlying file, not a modified buffer (this is done to avoid
+potential mistakes).  The return value of a modified buffer is
+the one prior to the modification, i.e. the one already written
+on disk.
+
+This command is useful for synchronizing multiple file names with
+their respective front matter." '(dired-mode) nil)
+
+(autoload 'denote-add-front-matter "denote" "\
+Insert front matter at the top of FILE.
+
+When called interactively, FILE is the return value of the
+function `buffer-file-name'.  FILE is checked to determine
+whether it is a note for Denote's purposes.
+
+TITLE is a string.  Interactively, it is the user input at the
+minibuffer prompt.
+
+KEYWORDS is a list of strings.  Interactively, it is the user
+input at the minibuffer prompt.  This one supports completion for
+multiple entries, each separated by the `crm-separator' (normally
+a comma).
+
+The purpose of this command is to help the user generate new
+front matter for an existing note (perhaps because the user
+deleted the previous one and could not undo the change).
+
+This command does not rename the file (e.g. to update the
+keywords).  To rename a file by reading its front matter as
+input, use `denote-rename-file-using-front-matter'.
+
+Note that this command is useful only for existing Denote notes.
+If the user needs to convert a generic text file to a Denote
+note, they can use one of the command which first rename the file
+to make it comply with our file-naming scheme and then add the
+relevant front matter.
+
+\(fn FILE TITLE KEYWORDS)" t nil)
+
+(autoload 'denote-dired-mode "denote" "\
+Fontify all Denote-style file names.
+Add this or `denote-dired-mode-in-directories' to
+`dired-mode-hook'.
 
 This is a minor mode.  If called interactively, toggle the
 `Denote-Dired mode' mode.  If the prefix argument is positive,
@@ -202,25 +269,11 @@ it is disabled.
 
 \(fn &optional ARG)" t nil)
 
-(autoload 'denote-dired-mode-in-directories "denote-dired" "\
+(autoload 'denote-dired-mode-in-directories "denote" "\
 Enable `denote-dired-mode' in `denote-dired-directories'.
 Add this function to `dired-mode-hook'." nil nil)
 
-(register-definition-prefixes "denote-dired" '("denote-dired-"))
-
-;;;***
-
-;;;### (autoloads nil "denote-faces" "denote-faces.el" (0 0 0 0))
-;;; Generated autoloads from denote-faces.el
-
-(register-definition-prefixes "denote-faces" '("denote-faces-"))
-
-;;;***
-
-;;;### (autoloads nil "denote-link" "denote-link.el" (0 0 0 0))
-;;; Generated autoloads from denote-link.el
-
-(autoload 'denote-link "denote-link" "\
+(autoload 'denote-link "denote" "\
 Create link to TARGET note in variable `denote-directory'.
 With optional ID-ONLY, such as a universal prefix
 argument (\\[universal-argument]), insert links with just the
@@ -229,10 +282,10 @@ format is always [[denote:IDENTIFIER]].
 
 \(fn TARGET &optional ID-ONLY)" t nil)
 
-(autoload 'denote-link-find-file "denote-link" "\
+(autoload 'denote-link-find-file "denote" "\
 Use minibuffer completion to visit linked file." t nil)
 
-(autoload 'denote-link-buttonize-buffer "denote-link" "\
+(autoload 'denote-link-buttonize-buffer "denote" "\
 Make denote: links actionable buttons in the current buffer.
 
 Add this to `find-file-hook'.  It will only work with Denote
@@ -245,7 +298,7 @@ positions, limit the process to the region in-between.
 
 \(fn &optional BEG END)" t nil)
 
-(autoload 'denote-link-backlinks "denote-link" "\
+(autoload 'denote-link-backlinks "denote" "\
 Produce a buffer with files linking to current note.
 Each file is a clickable/actionable button that visits the
 referenced entry.  Files are fontified if the user option
@@ -255,7 +308,7 @@ The placement of the backlinks' buffer is controlled by the user
 option `denote-link-backlinks-display-buffer-action'.  By
 default, it will show up below the current window." t nil)
 
-(autoload 'denote-link-add-links "denote-link" "\
+(autoload 'denote-link-add-links "denote" "\
 Insert links to all notes matching REGEXP.
 Use this command to reference multiple files at once.
 Particularly useful for the creation of metanotes (read the
@@ -266,7 +319,7 @@ inserts links with just the identifier.
 
 \(fn REGEXP &optional ID-ONLY)" t nil)
 
-(autoload 'denote-link-dired-marked-notes "denote-link" "\
+(autoload 'denote-link-dired-marked-notes "denote" "\
 Insert Dired marked FILES as links in BUFFER.
 
 FILES are Denote notes, meaning that they have our file-naming
@@ -288,17 +341,9 @@ This command is meant to be used from a Dired buffer.
 
 \(fn FILES BUFFER &optional ID-ONLY)" '(dired-mode) nil)
 
-(eval-after-load 'org `(funcall ',(lambda nil (with-no-warnings (org-link-set-parameters "denote" :follow #'denote-link-ol-follow :complete #'denote-link-ol-complete :export #'denote-link-ol-export)))))
+(eval-after-load 'org `(funcall ',(lambda nil (with-no-warnings (org-link-set-parameters "denote" :follow #'denote-link-ol-follow :face #'denote-link-ol-face :complete #'denote-link-ol-complete :export #'denote-link-ol-export)))))
 
-(register-definition-prefixes "denote-link" '("denote-link-"))
-
-;;;***
-
-;;;### (autoloads nil "denote-org-capture" "denote-org-capture.el"
-;;;;;;  (0 0 0 0))
-;;; Generated autoloads from denote-org-capture.el
-
-(autoload 'denote-org-capture "denote-org-capture" "\
+(autoload 'denote-org-capture "denote" "\
 Create new note through `org-capture-templates'.
 Use this as a function that returns the path to the new file.
 The file is populated with Denote's front matter.  It can then be
@@ -313,15 +358,56 @@ arbitrary text).
 
 Consult the manual for template samples." nil nil)
 
-(register-definition-prefixes "denote-org-capture" '("denote-org-capture-"))
+(autoload 'denote-migrate-old-org-filetags "denote" "\
+Rewrite Org filetags' value as colon-separated.
 
-;;;***
-
-;;;### (autoloads nil "denote-retrieve" "denote-retrieve.el" (0 0
-;;;;;;  0 0))
-;;; Generated autoloads from denote-retrieve.el
+Change the filetags from:
 
-(register-definition-prefixes "denote-retrieve" '("denote-retrieve--"))
+    #+filetags:   one  two
+
+To the standard format of:
+
+    #+filetags:  :one:two:
+
+A single tags chnages from TAG to :TAG:.
+
+Denote used to format filetags with two spaces between them, but
+this is not fully supported by Org.  The colon-separated entries
+are the rule.
+
+The rewrite DOES NOT SAVE BUFFERS.  The user is expected to
+review the changes, such as by using `diff-buffer-with-file'.
+Multiple buffers can be saved with `save-some-buffers' (check its
+doc string).
+
+This command is provided for the convenience of the user.  It
+shall be deprecated and eventually removed from future versions
+of Denote.  Written on 2022-08-10 for version 0.5.0." t nil)
+
+(autoload 'denote-migrate-old-markdown-yaml-tags "denote" "\
+Rewrite Markdown YAML tags value as comma-separated strings.
+
+Change the tags from:
+
+    tags:   one  two
+
+To the standard format of:
+
+    tags:  [\"one\", \"two\"]
+
+Denote used to format filetags with two spaces between them, but
+this is not supported by YAML.
+
+The rewrite DOES NOT SAVE BUFFERS.  The user is expected to
+review the changes, such as by using `diff-buffer-with-file'.
+Multiple buffers can be saved with `save-some-buffers' (check its
+doc string).
+
+This command is provided for the convenience of the user.  It
+shall be deprecated and eventually removed from future versions
+of Denote.  Written on 2022-08-10 for version 0.5.0." t nil)
+
+(register-definition-prefixes "denote" '("denote-"))
 
 ;;;***
 
