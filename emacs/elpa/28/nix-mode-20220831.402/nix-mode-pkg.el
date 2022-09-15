306332f27a74b756532aea5876f39f0223a82c13
gcv <gepardcv@gmail.com>
2022-09-15 07:57:18 -0700

    Emacs package update: nix-mode

--- emacs/elpa/28/nix-mode-20220831.402/nix-mode-pkg.el
+++ emacs/elpa/28/nix-mode-20220831.402/nix-mode-pkg.el
@@ -1,8 +1,8 @@
-(define-package "nix-mode" "20220719.505" "Major mode for editing .nix files"
+(define-package "nix-mode" "20220831.402" "Major mode for editing .nix files"
   '((emacs "25.1")
     (magit-section "0")
     (transient "0.3"))
-  :commit "e22819a73bf4e2b71166992a0af2f555036cf634" :maintainer
+  :commit "34d51e2731408b5b615f785a83faa3d6dc2a92a1" :maintainer
   '("Matthew Bauer" . "mjbauer95@gmail.com")
   :keywords
   '("nix" "languages" "tools" "unix")
