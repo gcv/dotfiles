ae0be638300a82ecc42376eee4b269896c3c5dbb
gcv <gepardcv@gmail.com>
2022-09-15 20:33:25 -0700

    Emacs package update: dirvish

--- emacs/elpa/27/dirvish-20220915.1457/dirvish-pkg.el
+++ emacs/elpa/27/dirvish-20220915.1457/dirvish-pkg.el
@@ -1,7 +1,7 @@
-(define-package "dirvish" "20220915.1154" "A modern file manager based on dired mode"
+(define-package "dirvish" "20220915.1457" "A modern file manager based on dired mode"
   '((emacs "27.1")
     (transient "0.3.7"))
-  :commit "5a7a797384e46dea71290c2adef62b6300b79c5d" :authors
+  :commit "49ffb1dba47165daeaf2d9b604ecfee78f6da98a" :authors
   '(("Alex Lu <https://github.com/alexluigit>"))
   :maintainer
   '("Alex Lu <https://github.com/alexluigit>")
