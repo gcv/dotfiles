d8d19ba6eaeb4df7940ce5818f0206c0be30f46b
gcv <gepardcv@gmail.com>
2022-09-15 20:33:23 -0700

    Emacs package update: rust-mode

--- emacs/elpa/27/rust-mode-20220825.1306/rust-mode-pkg.el
+++ emacs/elpa/27/rust-mode-20220825.1306/rust-mode-pkg.el
@@ -1,6 +1,6 @@
-(define-package "rust-mode" "20220819.1203" "A major-mode for editing Rust source code"
+(define-package "rust-mode" "20220825.1306" "A major-mode for editing Rust source code"
   '((emacs "25.1"))
-  :commit "894487d44c1664a9005cafd625fa99b54ff66c85" :authors
+  :commit "2d6bc53f526623e585aae5855a81a13b709df97d" :authors
   '(("Mozilla"))
   :maintainer
   '("Mozilla")
