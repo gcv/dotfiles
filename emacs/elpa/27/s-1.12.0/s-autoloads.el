21aa6961778aeb07c5849c356f1eb59ba08b4a47
gcv <gepardcv@gmail.com>
2022-09-15 20:33:29 -0700

    Emacs package update: s

--- emacs/elpa/27/s-1.12.0/s-autoloads.el
+++
@@ -1,17 +0,0 @@
-;;; s-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("../../../../../.emacs.d/elpa/s-1.12.0/s-autoloads.el"
-;;;;;;  "../../../../../.emacs.d/elpa/s-1.12.0/s.el") (23508 57446
-;;;;;;  540779 395000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; s-autoloads.el ends here
