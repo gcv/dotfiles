62148947b24e8a6e954aadceba4fc88ba9a2bb15
gcv <gepardcv@gmail.com>
2022-09-25 07:30:56 -0700

    Emacs package update: helpful

---
+++ emacs/elpa/28/helpful-20220919.540/helpful-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from helpful.el  -*- no-byte-compile: t -*-
+(define-package "helpful" "20220919.540" "A better *help* buffer" '((emacs "25") (dash "2.18.0") (s "1.11.0") (f "0.20.0") (elisp-refs "1.2")) :commit "db1e15b3783c83d9781a546f37b900ad53576659" :authors '(("Wilfred Hughes" . "me@wilfred.me.uk")) :maintainer '("Wilfred Hughes" . "me@wilfred.me.uk") :keywords '("help" "lisp") :url "https://github.com/Wilfred/helpful")
