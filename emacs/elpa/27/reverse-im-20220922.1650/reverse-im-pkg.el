b0ff174b4f60860ea427b885c1aeac1dae2f3a70
gcv <gepardcv@gmail.com>
2022-09-25 07:32:01 -0700

    Emacs package update: reverse-im

---
+++ emacs/elpa/27/reverse-im-20220922.1650/reverse-im-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from reverse-im.el  -*- no-byte-compile: t -*-
+(define-package "reverse-im" "20220922.1650" "Reverse mapping for non-default system layouts" '((emacs "25.1") (seq "2.23")) :commit "40e841b9d5b72ac0872de4e3f08406cb80236706" :keywords '("i18n") :url "https://github.com/a13/reverse-im.el")
