e585a26e8970d34546182c3a639feaed62656a55
gcv <gepardcv@gmail.com>
2022-09-25 07:30:57 -0700

    Emacs package update: embark

--- emacs/elpa/28/embark-20220920.1647/embark-pkg.el
+++ emacs/elpa/28/embark-20220920.1647/embark-pkg.el
@@ -1,6 +1,6 @@
-(define-package "embark" "20220913.1627" "Conveniently act on minibuffer completions"
+(define-package "embark" "20220920.1647" "Conveniently act on minibuffer completions"
   '((emacs "26.1"))
-  :commit "02c4b88f25fb4f4f17a553344875366117ccd2e1" :authors
+  :commit "2422d4e86f24b2b4204f2441f953e8407d011a2e" :authors
   '(("Omar Antolín Camarena" . "omar@matem.unam.mx"))
   :maintainer
   '("Omar Antolín Camarena" . "omar@matem.unam.mx")
