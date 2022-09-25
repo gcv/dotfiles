ab4dd74136c449fc43fae0bbb6e59ea1d3f27455
gcv <gepardcv@gmail.com>
2022-09-25 07:30:46 -0700

    Emacs package update: doom-themes

--- emacs/elpa/28/doom-themes-20220916.1056/doom-themes-pkg.el
+++ emacs/elpa/28/doom-themes-20220916.1056/doom-themes-pkg.el
@@ -1,7 +1,7 @@
-(define-package "doom-themes" "20220727.853" "an opinionated pack of modern color-themes"
+(define-package "doom-themes" "20220916.1056" "an opinionated pack of modern color-themes"
   '((emacs "25.1")
     (cl-lib "0.5"))
-  :commit "5f96ee83e9734587bf3e8ec61d4f067b43d9c26e" :authors
+  :commit "535800fd6ca7f5af56f7aa3d0e8f46fef8b7999b" :authors
   '(("Henrik Lissner" . "contact@henrik.io"))
   :maintainer
   '("Henrik Lissner" . "contact@henrik.io")
