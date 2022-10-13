f8ca51d40f51275b5fcb46e520187abeba356dcc
gcv <gepardcv@gmail.com>
2022-10-13 06:18:30 -0700

    Emacs package update: flycheck

--- emacs/elpa/27/flycheck-20220928.1355/flycheck-pkg.el
+++ emacs/elpa/27/flycheck-20220928.1355/flycheck-pkg.el
@@ -1,10 +1,10 @@
-(define-package "flycheck" "20220823.1254" "On-the-fly syntax checking"
+(define-package "flycheck" "20220928.1355" "On-the-fly syntax checking"
   '((dash "2.12.1")
     (pkg-info "0.4")
     (let-alist "1.0.4")
     (seq "1.11")
     (emacs "24.3"))
-  :commit "600b3bffda3862121d96bbc5c1f8990fa9033a82" :authors
+  :commit "d72c29fec85f1da6584475d88c1265f80e1275ad" :authors
   '(("Sebastian Wiesner" . "swiesner@lunaryorn.com"))
   :maintainer
   '("Clément Pit-Claudel" . "clement.pitclaudel@live.com")
