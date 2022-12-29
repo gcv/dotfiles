a095a347b6209ba419db99bb5ef15c98e04a5da4
gcv <gepardcv@gmail.com>
2022-12-28 17:18:42 -0800

    Emacs package update: swift-mode

--- emacs/elpa/28/swift-mode-20221119.910/swift-mode-pkg.el
+++ emacs/elpa/28/swift-mode-20221119.910/swift-mode-pkg.el
@@ -1,7 +1,7 @@
-(define-package "swift-mode" "20220924.648" "Major-mode for Apple's Swift programming language"
+(define-package "swift-mode" "20221119.910" "Major-mode for Apple's Swift programming language"
   '((emacs "24.4")
     (seq "2.3"))
-  :commit "f4529efa453f2f3051e2c8f772cd9da80147b93d" :authors
+  :commit "6d7093fb4a0b81686ffeff7e39f76027d83b7eb2" :authors
   '(("taku0" . "mxxouy6x3m_github@tatapa.org")
     ("Chris Barrett" . "chris.d.barrett@me.com")
     ("Bozhidar Batsov" . "bozhidar@batsov.com")
