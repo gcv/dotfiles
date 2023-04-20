f229140f685da36bad9e8840f51c36f77d78a0e0
gcv <gepardcv@gmail.com>
2023-04-19 22:25:33 -0700

    Emacs package update: js2-mode

--- emacs/elpa/28/js2-mode-20230408.115/js2-mode-pkg.el
+++ emacs/elpa/28/js2-mode-20230408.115/js2-mode-pkg.el
@@ -1,7 +1,7 @@
-(define-package "js2-mode" "20221028.1819" "Improved JavaScript editing mode"
+(define-package "js2-mode" "20230408.115" "Improved JavaScript editing mode"
   '((emacs "24.1")
     (cl-lib "0.5"))
-  :commit "a297ec2c2d9aa50ddc6c06959bb5a054e17b692a" :authors
+  :commit "7d928272bc311b1dd6f38d3f6365c18153e28636" :authors
   '(("Steve Yegge" . "steve.yegge@gmail.com")
     ("mooz" . "stillpedant@gmail.com")
     ("Dmitry Gutov" . "dgutov@yandex.ru"))
