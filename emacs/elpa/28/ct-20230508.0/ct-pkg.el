317b7fdaaa8a3afbec49b99808dfa4729f4a2b15
gcv <gepardcv@gmail.com>
2023-05-15 14:36:54 -0700

    Emacs package update: ct

--- emacs/elpa/28/ct-20230508.0/ct-pkg.el
+++ emacs/elpa/28/ct-20230508.0/ct-pkg.el
@@ -1,8 +1,10 @@
-(define-package "ct" "20230410.233" "Color Tools - a color api"
+(define-package "ct" "20230508.0" "Color Tools - a color api"
   '((emacs "26.1")
     (dash "2.18.0")
     (hsluv "1.0.0"))
-  :commit "86fe88af88b4accef559e8ae6e2bf2400e68a5ee" :authors
+  :commit "9e43c8587f25d3665f89487ad3e1cd85f3ef4dc4" :authors
+  '(("neeasade"))
+  :maintainers
   '(("neeasade"))
   :maintainer
   '("neeasade")
