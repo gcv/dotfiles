c766f2e105afee67c2fcbc3d80f9e4a4386fef26
gcv <gepardcv@gmail.com>
2023-09-11 11:35:48 -0700

    Emacs package update: solarized-theme

--- emacs/elpa/29/solarized-theme-2.0.3/solarized-theme-pkg.el
+++ emacs/elpa/29/solarized-theme-2.0.3/solarized-theme-pkg.el
@@ -1,6 +1,6 @@
-(define-package "solarized-theme" "2.0.1" "The Solarized color theme"
+(define-package "solarized-theme" "2.0.3" "The Solarized color theme"
   '((emacs "24.1"))
-  :commit "0f0b1129cf51c85904f9e015da4fcca5f5f7a4ce" :authors
+  :commit "07a681fff27ff22d268b726b05576ef93fc4e615" :authors
   '(("Bozhidar Batsov" . "bozhidar@batsov.dev"))
   :maintainers
   '(("Bozhidar Batsov" . "bozhidar@batsov.dev"))
