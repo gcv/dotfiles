8136d7b35c42038fa521e800a5715e0c9ff0c284
gcv <gepardcv@gmail.com>
2023-09-11 11:36:08 -0700

    Emacs package update: wfnames

---
+++ emacs/elpa/29/wfnames-1.1/wfnames-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from wfnames.el  -*- no-byte-compile: t -*-
+(define-package "wfnames" "1.1" "Edit filenames" '((emacs "24.3")) :commit "4447417c58b063df1fca1ba4bec9cb650c7beb60" :authors '(("Thierry Volpiatto" . "thievol@posteo.net")) :maintainer '("Thierry Volpiatto" . "thievol@posteo.net") :url "https://github.com/thierryvolpiatto/wfnames")
