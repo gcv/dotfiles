0e2453ae6f6deb26cf17c1c9d8d3a1a6e8c19912
gcv <gepardcv@gmail.com>
2023-09-11 11:35:53 -0700

    Emacs package update: leuven-theme

--- emacs/elpa/29/leuven-theme-20230812.1827/leuven-theme-pkg.el
+++ emacs/elpa/29/leuven-theme-20230812.1827/leuven-theme-pkg.el
@@ -1,4 +1,4 @@
-(define-package "leuven-theme" "20230523.841" "Elegant Emacs color theme for a white background" 'nil :commit "59123b1f0a4a1d195bf1726bf82371e3455470a8" :authors
+(define-package "leuven-theme" "20230812.1827" "Elegant Emacs color theme for a white background" 'nil :commit "b73a51a71ab8424bc2da8891f28e5239eac1236c" :authors
   '(("Fabrice Niessen <(concat \"fniessen\" at-sign \"pirilampo.org\")>"))
   :maintainers
   '(("Fabrice Niessen <(concat \"fniessen\" at-sign \"pirilampo.org\")>"))
