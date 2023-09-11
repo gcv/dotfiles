444d4b1f5cc8f93905018b7264167a7484a952fd
gcv <gepardcv@gmail.com>
2023-09-11 11:35:58 -0700

    Emacs package update: cmake-mode

---
+++ emacs/elpa/29/cmake-mode-3.27.4/cmake-mode-pkg.el
@@ -0,0 +1,6 @@
+(define-package "cmake-mode" "3.27.4" "major-mode for editing CMake sources"
+  '((emacs "24.1"))
+  :commit "c3977582b77f8b5a4ccacf460a262ff0a75d3cc5")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
