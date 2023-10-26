76fc8bfc35b679ea288462b42d084c3434f7c24a
gcv <gepardcv@gmail.com>
2023-10-26 14:45:18 -0700

    Emacs package update: chatgpt-shell

--- emacs/elpa/29/chatgpt-shell-20231024.1151/chatgpt-shell-pkg.el
+++ emacs/elpa/29/chatgpt-shell-20231024.1151/chatgpt-shell-pkg.el
@@ -1,7 +1,7 @@
-(define-package "chatgpt-shell" "20230911.2017" "ChatGPT shell + buffer insert commands"
+(define-package "chatgpt-shell" "20231024.1151" "ChatGPT shell + buffer insert commands"
   '((emacs "27.1")
     (shell-maker "0.42.1"))
-  :commit "8da666560551d6f8bd2f871c48e84b552836a023" :authors
+  :commit "311d732593d0b128a8bf69a8a70dc51b76731707" :authors
   '(("Alvaro Ramirez https://xenodium.com"))
   :maintainers
   '(("Alvaro Ramirez https://xenodium.com"))
