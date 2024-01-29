736d105416d5d2421a3482217f7b19d7978a1e55
gcv <gepardcv@gmail.com>
2024-01-29 14:54:09 -0800

    Emacs package update: slime

--- emacs/elpa/29/slime-2.29.1/slime-pkg.el
+++ emacs/elpa/29/slime-2.29.1/slime-pkg.el
@@ -1,7 +1,7 @@
-(define-package "slime" "2.28" "Superior Lisp Interaction Mode for Emacs"
+(define-package "slime" "2.29.1" "Superior Lisp Interaction Mode for Emacs"
   '((cl-lib "0.5")
     (macrostep "0.9"))
-  :commit "a4f3471487db48f7289dc0ea019611d093e5ee7f" :keywords
+  :commit "d85dd9899f7524513c8ad1bd47b0dd248b1915cb" :keywords
   '("languages" "lisp" "slime")
   :url "https://github.com/slime/slime")
 ;; Local Variables:
