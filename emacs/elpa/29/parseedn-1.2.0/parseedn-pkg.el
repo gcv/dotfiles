9b9a449b274a786ef0398c75b7b26f6ff6bd4384
gcv <gepardcv@gmail.com>
2024-01-29 14:54:01 -0800

    Emacs package update: parseedn

--- emacs/elpa/29/parseedn-1.2.0/parseedn-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from parseedn.el  -*- no-byte-compile: t -*-
-(define-package "parseedn" "1.2.0" "Clojure/EDN parser" '((emacs "26") (parseclj "1.1.0") (map "2")) :commit "c8f07926a688bfe995fde4460103915d401a1aff" :authors '(("Arne Brasseur" . "arne@arnebrasseur.net")) :maintainer '("Arne Brasseur" . "arne@arnebrasseur.net") :keywords '("lisp" "clojure" "edn" "parser") :url "http://www.github.com/clojure-emacs/parseedn")
