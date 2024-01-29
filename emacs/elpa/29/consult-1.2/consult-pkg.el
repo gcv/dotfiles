4a2467608bd76d3e2b9bce429a95b76881c21e16
gcv <gepardcv@gmail.com>
2024-01-29 14:54:02 -0800

    Emacs package update: consult

---
+++ emacs/elpa/29/consult-1.2/consult-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from consult.el  -*- no-byte-compile: t -*-
+(define-package "consult" "1.2" "Consulting completing-read" '((emacs "27.1") (compat "29.1.4.4")) :commit "ba1ccbb45cd76b3fae20844bd2950631a3312a81" :maintainer '("Daniel Mendler" . "mail@daniel-mendler.de") :keywords '("matching" "files" "completion") :url "https://github.com/minad/consult")
