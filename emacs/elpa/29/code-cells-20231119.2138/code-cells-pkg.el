2979300c4524ce29117d98be5a30f40e7809134e
gcv <gepardcv@gmail.com>
2024-01-29 14:53:52 -0800

    Emacs package update: code-cells

--- emacs/elpa/29/code-cells-20231119.2138/code-cells-pkg.el
+++ emacs/elpa/29/code-cells-20231119.2138/code-cells-pkg.el
@@ -1,6 +1,6 @@
-(define-package "code-cells" "20231015.1328" "Lightweight notebooks with support for ipynb files"
+(define-package "code-cells" "20231119.2138" "Lightweight notebooks with support for ipynb files"
   '((emacs "27.1"))
-  :commit "8a1f0281f88239b956250a8c76bada8aa9debe2c" :authors
+  :commit "44546ca256f3da29e3ac884e3d699c8455acbd6e" :authors
   '(("Augusto Stoffel" . "arstoffel@gmail.com"))
   :maintainers
   '(("Augusto Stoffel" . "arstoffel@gmail.com"))
