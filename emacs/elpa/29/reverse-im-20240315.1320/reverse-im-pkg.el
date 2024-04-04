7438be222a2c3fa903c3ecc577b1a436da046a24
gcv <gepardcv@gmail.com>
2024-04-04 10:46:26 -0700

    Emacs package update: reverse-im

---
+++ emacs/elpa/29/reverse-im-20240315.1320/reverse-im-pkg.el
@@ -0,0 +1,15 @@
+(define-package "reverse-im" "20240315.1320" "Reverse mapping for non-default system layouts"
+  '((emacs "25.1")
+    (seq "2.23"))
+  :commit "bcd70b49b16abab53165cb464d0a9a5f95bf946b" :authors
+  '(("Juri Linkov" . "juri@jurta.org"))
+  :maintainers
+  '(("DK" . "a13@users.noreply.github.com"))
+  :maintainer
+  '("DK" . "a13@users.noreply.github.com")
+  :keywords
+  '("i18n")
+  :url "https://github.com/a13/reverse-im.el")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
