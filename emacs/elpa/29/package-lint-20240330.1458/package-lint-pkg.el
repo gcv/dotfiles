9da93e1636e5824cea9630d762c9e2d09cce7f34
gcv <gepardcv@gmail.com>
2024-04-04 10:46:22 -0700

    Emacs package update: package-lint

--- emacs/elpa/29/package-lint-20240330.1458/package-lint-pkg.el
+++ emacs/elpa/29/package-lint-20240330.1458/package-lint-pkg.el
@@ -1,9 +1,7 @@
-(define-package "package-lint" "20240118.1725" "A linting library for elisp package authors"
-  '((cl-lib "0.5")
-    (emacs "24.4")
-    (let-alist "1.0.6")
-    (compat "29.1"))
-  :commit "3d74189df110b1656c2cd97c332921f4856dbd76" :authors
+(define-package "package-lint" "20240330.1458" "A linting library for elisp package authors"
+  '((emacs "24.4")
+    (let-alist "1.0.6"))
+  :commit "17e4ab20dd3e36fa540007b4f4047170c23bdb10" :authors
   '(("Steve Purcell" . "steve@sanityinc.com")
     ("Fanael Linithien" . "fanael4@gmail.com"))
   :maintainers
