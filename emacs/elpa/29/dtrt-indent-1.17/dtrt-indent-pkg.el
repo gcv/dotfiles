313716c7a84f1686de4cb46ac8df37086a88cbe3
gcv <gepardcv@gmail.com>
2024-04-04 10:46:23 -0700

    Emacs package update: dtrt-indent

--- emacs/elpa/29/dtrt-indent-1.17/dtrt-indent-pkg.el
+++ emacs/elpa/29/dtrt-indent-1.17/dtrt-indent-pkg.el
@@ -1,4 +1,4 @@
-(define-package "dtrt-indent" "1.16" "Adapt to foreign indentation offsets" 'nil :commit "0230ec503283b895bd3df6c1e30b35a01aa0b9af" :authors
+(define-package "dtrt-indent" "1.17" "Adapt to foreign indentation offsets" 'nil :commit "e45fa760eecf74edfa9e8419f9f9773d8a7abadd" :authors
   '(("Julian Scheid" . "julians37@googlemail.com"))
   :maintainers
   '(("Reuben Thomas" . "rrt@sc3d.org"))
