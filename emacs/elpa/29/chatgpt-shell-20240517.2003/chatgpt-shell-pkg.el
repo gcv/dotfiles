c845dbca5afe69337eaf3e85d39497931c7bba90
gcv <gepardcv@gmail.com>
2024-06-11 10:18:06 -0700

    Emacs package update: chatgpt-shell

--- emacs/elpa/29/chatgpt-shell-20240517.2003/chatgpt-shell-pkg.el
+++ emacs/elpa/29/chatgpt-shell-20240517.2003/chatgpt-shell-pkg.el
@@ -1,7 +1,7 @@
-(define-package "chatgpt-shell" "20240402.2216" "ChatGPT shell + buffer insert commands"
+(define-package "chatgpt-shell" "20240517.2003" "ChatGPT shell + buffer insert commands"
   '((emacs "27.1")
     (shell-maker "0.50.1"))
-  :commit "42cf53ba7d43e0622e8c4c57ef8f635e1248182b" :authors
+  :commit "910b0ad5cdb633f6be64f88596219a7eb58af226" :authors
   '(("Alvaro Ramirez https://xenodium.com"))
   :maintainers
   '(("Alvaro Ramirez https://xenodium.com"))
--- emacs/elpa/29/chatgpt-shell-20240517.2003/chatgpt-shell.el
+++ emacs/elpa/29/chatgpt-shell-20240517.2003/chatgpt-shell.el
@@ -4,7 +4,7 @@
 
 ;; Author: Alvaro Ramirez https://xenodium.com
 ;; URL: https://github.com/xenodium/chatgpt-shell
-;; Version: 1.0.8
+;; Version: 1.0.9
 ;; Package-Requires: ((emacs "27.1") (shell-maker "0.50.1"))
 
 ;; This package is free software; you can redistribute it and/or modify
@@ -209,7 +209,8 @@ Can be used compile or run source block at point."
   :group 'chatgpt-shell)
 
 (defcustom chatgpt-shell-model-versions
-  '("gpt-4-0125-preview"
+  '("gpt-4o"
+    "gpt-4-0125-preview"
     "gpt-4-turbo-preview"
     "gpt-4-1106-preview"
     "gpt-4-0613"
@@ -1934,6 +1935,8 @@ For example:
   "Build ChatGPT curl command list using REQUEST-DATA."
   (let ((json-path (chatgpt-shell--json-request-file)))
     (with-temp-file json-path
+      (when (eq system-type 'windows-nt)
+        (setq-local buffer-file-coding-system 'utf-8))
       (insert (shell-maker--json-encode request-data)))
     (append (list "curl" (chatgpt-shell--api-url))
             chatgpt-shell-additional-curl-options
