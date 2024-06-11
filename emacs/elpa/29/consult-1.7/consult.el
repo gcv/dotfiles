62742ae970630f7c1d691125e6ad5976151bdfff
gcv <gepardcv@gmail.com>
2024-06-11 10:18:26 -0700

    Emacs package update: consult

--- emacs/elpa/29/consult-1.7/consult.el
+++ emacs/elpa/29/consult-1.7/consult.el
@@ -5,7 +5,7 @@
 ;; Author: Daniel Mendler and Consult contributors
 ;; Maintainer: Daniel Mendler <mail@daniel-mendler.de>
 ;; Created: 2020
-;; Version: 1.4
+;; Version: 1.7
 ;; Package-Requires: ((emacs "27.1") (compat "29.1.4.4"))
 ;; Homepage: https://github.com/minad/consult
 ;; Keywords: matching, files, completion
@@ -279,7 +279,7 @@ Can be either a string, or a list of strings or expressions."
   :type '(choice string (repeat (choice string sexp))))
 
 (defcustom consult-find-args
-  "find . -not ( -iwholename */.[a-z]* -prune )"
+  "find . -not ( -path */.[A-Za-z]* -prune )"
   "Command line arguments for find, see `consult-find'.
 The dynamically computed arguments are appended.
 Can be either a string, or a list of strings or expressions."
@@ -340,9 +340,11 @@ chunk from the beginning of the file is previewed."
   :type '(repeat regexp))
 
 (defcustom consult-preview-allowed-hooks
-  '(global-font-lock-mode-check-buffers
+  '(global-font-lock-mode
     save-place-find-file-hook)
-  "List of `find-file' hooks, which should be executed during file preview."
+  "List of hooks, which should be executed during file preview.
+This variable applies to `find-file-hook', `change-major-mode-hook' and
+mode hooks, e.g., `prog-mode-hook'."
   :type '(repeat symbol))
 
 (defcustom consult-preview-variables
@@ -591,7 +593,7 @@ We use invalid characters outside the Unicode range.")
               (setq result
                     (mapcar
                      (lambda (x)
-                       (if (and (string-suffix-p (symbol-name (car-safe x)) "-function") (cdr x))
+                       (if (and (string-suffix-p "-function" (symbol-name (car-safe x))) (cdr x))
                            (cons (car x) (consult--in-buffer (cdr x)))
                          x))
                      result)))
@@ -697,7 +699,7 @@ The line beginning/ending BEG/END is bound in BODY."
   (declare (indent 2))
   (cl-with-gensyms (max)
     `(save-excursion
-       (let ((,beg (point-min)) (,max (point-max)) end)
+       (let ((,beg (point-min)) (,max (point-max)) ,end)
          (while (< ,beg ,max)
            (goto-char ,beg)
            (setq ,end (pos-eol))
@@ -1263,14 +1265,29 @@ Return the location marker."
 
 ;;;; Preview support
 
+(defun consult--preview-allowed-p (fun)
+  "Return non-nil if FUN is an allowed preview mode hook."
+  (or (memq fun consult-preview-allowed-hooks)
+      (when-let (((symbolp fun))
+                 (name (symbol-name fun))
+                 ;; Global modes in Emacs 29 are activated via a
+                 ;; `find-file-hook' ending with `-check-buffers'. This has been
+                 ;; changed in Emacs 30. Now a `change-major-mode-hook' is used
+                 ;; instead with the suffix `-check-buffers'.
+                 (suffix (if (eval-when-compile (>= emacs-major-version 30))
+                             "-enable-in-buffer"
+                           "-check-buffers"))
+                 ((string-suffix-p suffix name)))
+        (memq (intern (string-remove-suffix suffix name))
+              consult-preview-allowed-hooks))))
+
 (defun consult--filter-find-file-hook (orig &rest hooks)
   "Filter `find-file-hook' by `consult-preview-allowed-hooks'.
 This function is an advice for `run-hooks'.
 ORIG is the original function, HOOKS the arguments."
   (if (memq 'find-file-hook hooks)
       (cl-letf* (((default-value 'find-file-hook)
-                  (seq-filter (lambda (x)
-                                (memq x consult-preview-allowed-hooks))
+                  (seq-filter #'consult--preview-allowed-p
                               (default-value 'find-file-hook)))
                  (find-file-hook (default-value 'find-file-hook)))
         (apply orig hooks))
@@ -1313,6 +1330,12 @@ ORIG is the original function, HOOKS the arguments."
             (when (bound-and-true-p so-long-detected-p)
               (error "No preview of file `%s' with long lines"
                      (file-name-nondirectory name)))
+            ;; Run delayed hooks listed in `consult-preview-allowed-hooks'.
+            (dolist (hook (reverse (cons 'after-change-major-mode-hook delayed-mode-hooks)))
+              (run-hook-wrapped hook (lambda (fun)
+                                       (when (consult--preview-allowed-p fun)
+                                         (funcall fun))
+                                       nil)))
             (setq success (current-buffer)))
         (unless success
           (kill-buffer buffer))))))
@@ -1366,7 +1389,7 @@ ORIG is the original function, HOOKS the arguments."
                   (kill-buffer buf))
                 (setq temporary-buffers nil)
                 (pcase-dolist (`(,file . ,wins) live-files)
-                  (when-let (buf (find-file-noselect file))
+                  (when-let (buf (consult--file-action file))
                     (push buf orig-buffers)
                     (pcase-dolist (`(,win . ,state) wins)
                       (setf (car (alist-get 'buffer state)) buf)
@@ -2808,13 +2831,15 @@ KEYMAP is a command-specific keymap."
              (items (plist-get src :items))
              (items (if (functionp items) (funcall items) items)))
         (dolist (item items)
-          (let ((cand (consult--tofu-append item idx)))
+          (let* ((str (or (car-safe item) item))
+                 (cand (consult--tofu-append str idx)))
             ;; Preserve existing `multi-category' datum of the candidate.
-            (if (get-text-property 0 'multi-category cand)
-                (when face (add-text-properties 0 (length item) face cand))
+            (if (and (eq str item) (get-text-property 0 'multi-category str))
+                (when face (add-text-properties 0 (length str) face cand))
               ;; Attach `multi-category' datum and face.
-              (add-text-properties 0 (length item)
-                                   `(multi-category (,cat . ,item) ,@face) cand))
+              (add-text-properties
+               0 (length str)
+               `(multi-category (,cat . ,(or (cdr-safe item) item)) ,@face) cand))
             (push cand candidates))))
       (cl-incf idx))
     (nreverse candidates)))
@@ -3719,7 +3744,10 @@ command respects narrowing and the settings
 
 (defun consult--file-action (file)
   "Open FILE via `consult--buffer-action'."
-  (consult--buffer-action (find-file-noselect file)))
+  ;; Try to preserve the buffer as is, if it has already been opened, for
+  ;; example in literal or raw mode.
+  (setq file (abbreviate-file-name (expand-file-name file)))
+  (consult--buffer-action (or (get-file-buffer file) (find-file-noselect file))))
 
 (consult--define-state file)
 
@@ -4347,14 +4375,14 @@ The command supports previewing the currently selected theme."
 (defun consult--buffer-sort-visibility (buffers)
   "Sort BUFFERS by visibility."
   (let ((hidden)
-        (current (current-buffer)))
+        (current (car (memq (current-buffer) buffers))))
     (consult--keep! buffers
       (unless (eq it current)
         (if (get-buffer-window it 'visible)
             it
           (push it hidden)
           nil)))
-    (nconc (nreverse hidden) buffers (list current))))
+    (nconc (nreverse hidden) buffers (and current (list current)))))
 
 (defun consult--normalize-directory (dir)
   "Normalize directory DIR.
@@ -4382,7 +4410,8 @@ to search and is passed to `consult--buffer-query'."
           buffers)))
 
 (cl-defun consult--buffer-query (&key sort directory mode as predicate (filter t)
-                                      include (exclude consult-buffer-filter))
+                                      include (exclude consult-buffer-filter)
+                                      (buffer-list t))
   "Query for a list of matching buffers.
 The function supports filtering by various criteria which are
 used throughout Consult.  In particular it is the backbone of
@@ -4394,16 +4423,17 @@ EXCLUDE is a list of regexps.
 INCLUDE is a list of regexps.
 MODE can be a mode or a list of modes to restrict the returned buffers.
 PREDICATE is a predicate function.
+BUFFER-LIST is the unfiltered list of buffers.
 AS is a conversion function."
-  (let ((root (consult--normalize-directory directory))
-        (buffers (buffer-list)))
+  (let ((root (consult--normalize-directory directory)))
+    (setq buffer-list (if (eq buffer-list t) (buffer-list) (copy-sequence buffer-list)))
     (when sort
-      (setq buffers (funcall (intern (format "consult--buffer-sort-%s" sort)) buffers)))
+      (setq buffer-list (funcall (intern (format "consult--buffer-sort-%s" sort)) buffer-list)))
     (when (or filter mode as root)
       (let ((exclude-re (consult--regexp-filter exclude))
             (include-re (consult--regexp-filter include))
             (case-fold-search))
-        (consult--keep! buffers
+        (consult--keep! buffer-list
           (and
            (or (not mode)
                (let ((mm (buffer-local-value 'major-mode it)))
@@ -4427,11 +4457,15 @@ AS is a conversion function."
                                     (expand-file-name dir)))))
            (or (not predicate) (funcall predicate it))
            (if as (funcall as it) it)))))
-    buffers))
+    buffer-list))
 
 (defun consult--buffer-file-hash ()
   "Return hash table of all buffer file names."
   (consult--string-hash (consult--buffer-query :as #'buffer-file-name)))
+
+(defun consult--buffer-pair (buffer)
+  "Return a pair of name of BUFFER and BUFFER."
+  (cons (buffer-name buffer) buffer))
 
 (defun consult--buffer-preview ()
   "Buffer preview function."
@@ -4488,7 +4522,7 @@ If NORECORD is non-nil, do not record the buffer switch in the buffer list."
        (when-let (root (consult--project-root))
          (consult--buffer-query :sort 'visibility
                                 :directory root
-                                :as #'buffer-name))))
+                                :as #'consult--buffer-pair))))
   "Project buffer candidate source for `consult-buffer'.")
 
 (defvar consult--source-project-recent-file
@@ -4545,7 +4579,7 @@ If NORECORD is non-nil, do not record the buffer switch in the buffer list."
     :items
     ,(lambda () (consult--buffer-query :sort 'visibility
                                        :filter 'invert
-                                       :as #'buffer-name)))
+                                       :as #'consult--buffer-pair)))
   "Hidden buffer candidate source for `consult-buffer'.")
 
 (defvar consult--source-modified-buffer
@@ -4558,7 +4592,7 @@ If NORECORD is non-nil, do not record the buffer switch in the buffer list."
     :state    ,#'consult--buffer-state
     :items
     ,(lambda () (consult--buffer-query :sort 'visibility
-                                       :as #'buffer-name
+                                       :as #'consult--buffer-pair
                                        :predicate
                                        (lambda (buf)
                                          (and (buffer-modified-p buf)
@@ -4575,7 +4609,7 @@ If NORECORD is non-nil, do not record the buffer switch in the buffer list."
     :default  t
     :items
     ,(lambda () (consult--buffer-query :sort 'visibility
-                                       :as #'buffer-name)))
+                                       :as #'consult--buffer-pair)))
   "Buffer candidate source for `consult-buffer'.")
 
 (defun consult--file-register-p (reg)
@@ -4742,7 +4776,7 @@ FIND-FILE is the file open function, defaulting to `find-file-noselect'."
            (file (substring-no-properties cand 0 file-end))
            (line (string-to-number (substring-no-properties cand (+ 1 file-end) line-end))))
       (when-let (pos (consult--marker-from-line-column
-                      (funcall (or find-file #'find-file-noselect) file)
+                      (funcall (or find-file #'consult--file-action) file)
                       line (or (car matches) 0)))
         (cons pos (cdr matches))))))
 
@@ -5009,7 +5043,7 @@ regarding the asynchronous search and the arguments."
           (pcase-let ((`(,re . ,hl) (funcall consult--regexp-compiler arg 'pcre ignore-case)))
             (when re
               (cons (append cmd
-                            (cdr (mapcan (lambda (x) `("--and" ,x)) re))
+                            (mapcan (lambda (x) `("--and" ,x)) re)
                             opts paths)
                     hl))))))))
 
