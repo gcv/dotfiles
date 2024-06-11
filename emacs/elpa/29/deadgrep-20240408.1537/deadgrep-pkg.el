3698954e77e2cdc745f213b871547e9ce417cdd1
gcv <gepardcv@gmail.com>
2024-06-11 10:18:22 -0700

    Emacs package update: deadgrep

--- emacs/elpa/29/deadgrep-20240408.1537/deadgrep-pkg.el
+++ emacs/elpa/29/deadgrep-20240408.1537/deadgrep-pkg.el
@@ -1,9 +1,9 @@
-(define-package "deadgrep" "20231130.1628" "fast, friendly searching with ripgrep"
+(define-package "deadgrep" "20240408.1537" "fast, friendly searching with ripgrep"
   '((emacs "25.1")
     (dash "2.12.0")
     (s "1.11.0")
     (spinner "1.7.3"))
-  :commit "38abe362997d2f18633a75d04c09da751bf8085e" :authors
+  :commit "eafc642c551e6d5df7eb1fee9aa7596e59811178" :authors
   '(("Wilfred Hughes" . "me@wilfred.me.uk"))
   :maintainers
   '(("Wilfred Hughes" . "me@wilfred.me.uk"))
