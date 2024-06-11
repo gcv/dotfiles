8770ac7f46328e9dc64628adf6d9fa27e03d41a6
gcv <gepardcv@gmail.com>
2024-06-11 10:18:25 -0700

    Emacs package update: posframe

---
+++ emacs/elpa/29/posframe-1.4.3/posframe-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from posframe.el  -*- no-byte-compile: t -*-
+(define-package "posframe" "1.4.3" "Pop a posframe (just a frame) at point" '((emacs "26.1")) :commit "468ac1ab50d7e0feae2c06f12596bbc169f2abe4" :authors '(("Feng Shu" . "tumashu@163.com")) :maintainer '("Feng Shu" . "tumashu@163.com") :keywords '("convenience" "tooltip") :url "https://github.com/tumashu/posframe")
