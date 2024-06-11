341a0a66fc11f0800a61402b23a5b7b8c278ff34
gcv <gepardcv@gmail.com>
2024-06-11 10:18:10 -0700

    Emacs package update: dired-sidebar

--- emacs/elpa/29/dired-sidebar-20240522.2316/dired-sidebar-pkg.el
+++ emacs/elpa/29/dired-sidebar-20240522.2316/dired-sidebar-pkg.el
@@ -1,7 +1,7 @@
-(define-package "dired-sidebar" "20240320.1952" "Tree browser leveraging dired"
+(define-package "dired-sidebar" "20240522.2316" "Tree browser leveraging dired"
   '((emacs "25.1")
     (dired-subtree "0.0.1"))
-  :commit "573658021fd6e6cf032d789de29aca4e5dd258fd" :authors
+  :commit "702165ad53a473992d84e0207b984b9be5114bde" :authors
   '(("James Nguyen" . "james@jojojames.com"))
   :maintainers
   '(("James Nguyen" . "james@jojojames.com"))
