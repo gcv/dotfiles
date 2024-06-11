bef24ee1859a6d2b66b1ab74e930eb59b18f0fc2
gcv <gepardcv@gmail.com>
2024-06-11 10:18:13 -0700

    Emacs package update: doom-themes

--- emacs/elpa/29/doom-themes-20240404.2042/doom-themes-pkg.el
+++ emacs/elpa/29/doom-themes-20240404.2042/doom-themes-pkg.el
@@ -1,7 +1,7 @@
-(define-package "doom-themes" "20240320.121" "an opinionated pack of modern color-themes"
+(define-package "doom-themes" "20240404.2042" "an opinionated pack of modern color-themes"
   '((emacs "25.1")
     (cl-lib "0.5"))
-  :commit "37d2182f5078d29fb22bbace346e523bb84b2c4e" :authors
+  :commit "3b2422b208d28e8734b300cd3cc6a7f4af5eba55" :authors
   '(("Henrik Lissner" . "contact@henrik.io"))
   :maintainers
   '(("Henrik Lissner" . "contact@henrik.io"))
