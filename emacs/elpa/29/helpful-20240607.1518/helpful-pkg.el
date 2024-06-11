0861e465867bbd61a23727e8d9ab08fbc15cbd58
gcv <gepardcv@gmail.com>
2024-06-11 10:18:27 -0700

    Emacs package update: helpful

--- emacs/elpa/29/helpful-20240607.1518/helpful-pkg.el
+++ emacs/elpa/29/helpful-20240607.1518/helpful-pkg.el
@@ -1,10 +1,10 @@
-(define-package "helpful" "20231028.516" "A better *help* buffer"
+(define-package "helpful" "20240607.1518" "A better *help* buffer"
   '((emacs "25")
     (dash "2.18.0")
     (s "1.11.0")
     (f "0.20.0")
     (elisp-refs "1.2"))
-  :commit "a32a5b3d959a7fccf09a71d97b3d7c888ac31c69" :authors
+  :commit "a835b7ec59004a13f4a09ff127a785a92b957e8f" :authors
   '(("Wilfred Hughes" . "me@wilfred.me.uk"))
   :maintainers
   '(("Wilfred Hughes" . "me@wilfred.me.uk"))
