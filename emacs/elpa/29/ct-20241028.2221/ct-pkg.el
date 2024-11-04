3280177e2dba85606da1cfddbfe9663bcaf8f553
gcv <gepardcv@gmail.com>
2024-11-04 06:42:08 -0800

    Emacs package update: ct

---
+++ emacs/elpa/29/ct-20241028.2221/ct-pkg.el
@@ -0,0 +1,10 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "ct" "20241028.2221"
+  "Color Tools - a color api."
+  '((emacs "26.1")
+    (dash  "2.18.0")
+    (hsluv "1.0.0"))
+  :url "https://github.com/neeasade/ct.el"
+  :commit "6d810601d92cd3b41ede71ab1c9a50de955c71ca"
+  :revdesc "6d810601d92c"
+  :keywords '("convenience" "color" "theming" "rgb" "hsv" "hsl" "lab" "oklab" "background"))
