c08798eada9c940db99e270ab8260ca635291f72
gcv <gepardcv@gmail.com>
2024-11-04 06:41:34 -0800

    Emacs package update: git-commit

---
+++ emacs/elpa/29/git-commit-4.1.2/git-commit-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "git-commit" "4.1.2"
+  "Edit Git commit messages."
+  ()
+  :url "https://github.com/magit/magit"
+  :commit "4992c3d1f64e0e983692c7a61d47069f47380dbf"
+  :revdesc "v4.1.2-0-g4992c3d1f64e"
+  :authors '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")
+             ("Sebastian Wiesner" . "lunaryorn@gmail.com")
+             ("Florian Ragwitz" . "rafl@debian.org")
+             ("Marius Vollmer" . "marius.vollmer@gmail.com"))
+  :maintainers '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")))
