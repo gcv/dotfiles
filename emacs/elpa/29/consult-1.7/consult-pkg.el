1f04bb368c57c0c5d5c635773d64164e5769c84f
gcv <gepardcv@gmail.com>
2024-11-04 06:41:52 -0800

    Emacs package update: consult

--- emacs/elpa/29/consult-1.7/consult-pkg.el
+++
@@ -1,2 +0,0 @@
-;; Generated package description from consult.el  -*- no-byte-compile: t -*-
-(define-package "consult" "1.7" "Consulting completing-read" '((emacs "27.1") (compat "29.1.4.4")) :commit "fe4852280006e61be7f1374d021ee06155ce5a26" :maintainer '("Daniel Mendler" . "mail@daniel-mendler.de") :keywords '("matching" "files" "completion") :url "https://github.com/minad/consult")
---
+++ emacs/elpa/29/consult-1.8/consult-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from consult.el  -*- no-byte-compile: t -*-
+(define-package "consult" "1.8" "Consulting completing-read" '((emacs "27.1") (compat "30")) :commit "4889458dccf842ab6223099f8a73ff8b147e9459" :maintainer '("Daniel Mendler" . "mail@daniel-mendler.de") :keywords '("matching" "files" "completion") :url "https://github.com/minad/consult")
