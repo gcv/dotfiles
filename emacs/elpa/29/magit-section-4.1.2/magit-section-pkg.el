521c540d676e2c01358cb4a5a01d4461c45dbde9
gcv <gepardcv@gmail.com>
2024-11-04 06:41:21 -0800

    Emacs package update: magit-section

---
+++ emacs/elpa/29/magit-section-4.1.2/magit-section-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from magit-section.el  -*- no-byte-compile: t -*-
+(define-package "magit-section" "4.1.2" "Sections for read-only buffers" '((emacs "26.1") (compat "30.0.0.0") (dash "2.19.1") (seq "2.24")) :commit "4992c3d1f64e0e983692c7a61d47069f47380dbf" :authors '(("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev")) :maintainer '("Jonas Bernoulli" . "emacs.magit@jonas.bernoulli.dev") :keywords '("tools") :url "https://github.com/magit/magit")
