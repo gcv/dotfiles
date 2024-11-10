23fbc3351977188ca9dcc88ff261eb729a721d2f
Constantine Vetoshev <vetoshev@gmail.com>
2024-11-10 14:11:26 -0800

    Install Snail from MELPA.

---
+++ emacs/elpa/29/julia-snail-20240812.840/julia-snail-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "julia-snail" "20240812.840"
+  "Julia Snail."
+  '((emacs      "26.2")
+    (dash       "2.16.0")
+    (julia-mode "0.3")
+    (s          "1.12.0")
+    (spinner    "1.7.3")
+    (popup      "0.5.9"))
+  :url "https://github.com/gcv/julia-snail"
+  :commit "dff92c4250e40a6cc106f0ea993f9631ad55eb7c"
+  :revdesc "dff92c4250e4")
