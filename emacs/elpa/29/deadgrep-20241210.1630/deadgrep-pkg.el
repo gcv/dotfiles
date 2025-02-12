c6c64e93a481a4ac66b98d27a90220853a074d5f
Constantine Vetoshev <vetoshev@gmail.com>
2025-02-12 07:03:46 -0800

    Emacs package update: deadgrep

--- emacs/elpa/29/deadgrep-20241210.1630/deadgrep-pkg.el
+++ emacs/elpa/29/deadgrep-20241210.1630/deadgrep-pkg.el
@@ -1,13 +1,13 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "deadgrep" "20241030.1444"
+(define-package "deadgrep" "20241210.1630"
   "Fast, friendly searching with ripgrep."
   '((emacs   "25.1")
     (dash    "2.12.0")
     (s       "1.11.0")
     (spinner "1.7.3"))
   :url "https://github.com/Wilfred/deadgrep"
-  :commit "2f4ac50e297ce6fe219606f4709ad6cab4436914"
-  :revdesc "2f4ac50e297c"
+  :commit "bb555790c6f404572d537e1e4adec8b4ff0515f5"
+  :revdesc "bb555790c6f4"
   :keywords '("tools")
   :authors '(("Wilfred Hughes" . "me@wilfred.me.uk"))
   :maintainers '(("Wilfred Hughes" . "me@wilfred.me.uk")))
