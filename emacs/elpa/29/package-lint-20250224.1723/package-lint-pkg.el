68779786b0a90639c235fe7484aba64530722baa
Constantine Vetoshev <vetoshev@gmail.com>
2025-02-26 12:33:01 -0800

    Emacs package update: package-lint

--- emacs/elpa/29/package-lint-20250224.1723/package-lint-pkg.el
+++ emacs/elpa/29/package-lint-20250224.1723/package-lint-pkg.el
@@ -1,11 +1,11 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "package-lint" "20250108.1407"
+(define-package "package-lint" "20250224.1723"
   "A linting library for elisp package authors."
   '((emacs     "24.4")
     (let-alist "1.0.6"))
   :url "https://github.com/purcell/package-lint"
-  :commit "6f05a369e0718e93c5dce0951cad5e6646296612"
-  :revdesc "6f05a369e071"
+  :commit "b5deebdeb6eeb12a99ab7f1fad8ddf51ee7dcfb5"
+  :revdesc "b5deebdeb6ee"
   :keywords '("lisp")
   :authors '(("Steve Purcell" . "steve@sanityinc.com")
              ("Fanael Linithien" . "fanael4@gmail.com"))
