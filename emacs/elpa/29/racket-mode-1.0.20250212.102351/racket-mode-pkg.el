ce4136287ad41041511ea56d169c1abed38bc438
Constantine Vetoshev <vetoshev@gmail.com>
2025-02-26 12:33:02 -0800

    Emacs package update: racket-mode

---
+++ emacs/elpa/29/racket-mode-1.0.20250212.102351/racket-mode-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from racket-mode.el  -*- no-byte-compile: t -*-
+(define-package "racket-mode" "1.0.20250212.102351" "Racket editing, REPL, and more" '((emacs "25.1")) :commit "HEAD" :authors '(("Greg Hendershott" . "racket-mode-author@greghendershott.com")) :url "https://www.racket-mode.com/")
