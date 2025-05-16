1de76178e6fdbeca887d1544889eb8202ce7ad7b
Constantine Vetoshev <vetoshev@gmail.com>
2025-05-16 06:28:21 -0700

    Emacs package update: dash

--- emacs/elpa/30/dash-2.20.0/dash-pkg.el
+++ emacs/elpa/30/dash-2.20.0/dash-pkg.el
@@ -1,10 +1,10 @@
 ;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "dash" "2.19.1"
+(define-package "dash" "2.20.0"
   "A modern list library for Emacs."
   '((emacs "24"))
   :url "https://github.com/magnars/dash.el"
-  :commit "39d067b9fbb2db65fc7a6938bfb21489ad990cb4"
-  :revdesc "2.19.1-0-g39d067b9fbb2"
+  :commit "fcb5d831fc08a43f984242c7509870f30983c27c"
+  :revdesc "2.20.0-0-gfcb5d831fc08"
   :keywords '("extensions" "lisp")
   :authors '(("Magnar Sveen" . "magnars@gmail.com"))
-  :maintainers '(("Magnar Sveen" . "magnars@gmail.com")))
+  :maintainers '(("Basil L. Contovounesios" . "basil@contovou.net")))
