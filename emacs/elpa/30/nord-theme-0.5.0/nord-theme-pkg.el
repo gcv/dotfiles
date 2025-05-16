6269deb2d92f01a9afe6ab30bf5f1442981ebe41
Constantine Vetoshev <vetoshev@gmail.com>
2025-05-16 06:28:03 -0700

    Emacs package update: nord-theme

--- emacs/elpa/30/nord-theme-0.5.0/nord-theme-pkg.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; lexical-binding: nil -*-
-(define-package "nord-theme" "0.5.0"
-  "An arctic, north-bluish clean and elegant theme."
-  '((emacs "24"))
-  :url "https://github.com/arcticicestudio/nord-emacs"
-  :commit "0f5295f99005a200191ce7b660e56cd0510cf710"
-  :revdesc "v0.5.0-0-g0f5295f99005"
-  :authors '(("Arctic Ice Studio" . "development@arcticicestudio.com"))
-  :maintainers '(("Arctic Ice Studio" . "development@arcticicestudio.com")))
---
+++ emacs/elpa/30/nord-theme-0.6.0/nord-theme-pkg.el
@@ -0,0 +1,9 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "nord-theme" "0.6.0"
+  "An arctic, north-bluish clean and elegant theme."
+  '((emacs "24"))
+  :url "https://github.com/nordtheme/emacs"
+  :commit "551b2b8a0751c0a22e5c5daa6958152f208e668f"
+  :revdesc "v0.6.0-0-g551b2b8a0751"
+  :authors '(("Sven Greb" . "development@svengreb.de"))
+  :maintainers '(("Sven Greb" . "development@svengreb.de")))
