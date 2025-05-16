a229f9e91e235de33811b8bb707f9c097f92a647
Constantine Vetoshev <vetoshev@gmail.com>
2025-05-16 06:28:18 -0700

    Emacs package update: llama

---
+++ emacs/elpa/30/llama-0.6.2/llama-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from llama.el  -*- mode: lisp-data; no-byte-compile: t -*-
+(define-package "llama" "0.6.2" "Compact syntax for short lambda" '((emacs "26.1") (compat "30.0.2.0")) :commit "48e5bc4919a4a29665362832d59ade8e248b0c3e" :keywords '("extensions") :url "https://github.com/tarsius/llama")
